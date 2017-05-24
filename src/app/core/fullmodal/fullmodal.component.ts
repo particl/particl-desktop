b013557507889ae8dd2c85b65cd16f92562e68ec
Jason Eybers <jasoneybers@gmail.com>
2017-05-24 12:03:49 +0200

    added styling to modals, updated syncing bar to be dynamic

--- src/app/core/fullmodal/fullmodal.component.ts
+++ src/app/core/fullmodal/fullmodal.component.ts
@@ -7,12 +7,14 @@ import { Component, Input, OnDestroy, Renderer, HostListener, ElementRef, ViewCh
 })
 export class FullmodalComponent implements OnDestroy {
   @ViewChild('modal') modal:ElementRef;
+  @ViewChild('percentageBar') percentageBar:ElementRef;
 
   @Input() showCloseButton: boolean = true;
   @Input() isOpen: boolean = false;
   @Input() closeOnEscape: boolean = true;
 
   private hasScrollY: boolean = false;
+  public syncPercentage: number = 0.00;
   private synced: boolean = false;
 
   constructor(private elementRef: ElementRef,
@@ -20,28 +22,9 @@ export class FullmodalComponent implements OnDestroy {
     this.renderer.setElementClass(document.body, 'modal-open', true);
   }
 
-  ngOnDestroy() {
-    this.renderer.setElementClass(document.body, 'modal-open', false);
-  }
-
-  ngDoCheck() {
-    this.renderer.setElementClass(document.body, 'modal-open', this.isOpen);
-
-    if (this.isOpen && this.modal) {
-      let element = this.modal.nativeElement;
-      let style = element.ownerDocument.defaultView.getComputedStyle(element, undefined);
-
-      this.hasScrollY = style.overflowY === 'scroll' || (style.overflowY === 'auto' && element.clientHeight < element.scrollHeight);
-    }
-
-  }
-
-  ngAfterViewInit() {
-    this.ngDoCheck()
-  }
-
   open() {
     this.isOpen = true;
+    this.ngDoCheck();
   }
 
   close() {
@@ -52,5 +35,26 @@ export class FullmodalComponent implements OnDestroy {
   keyboardInput(event: any) {
     if(this.closeOnEscape && event.code.toLowerCase()=="escape")
       this.isOpen = false;
+  }
+
+  ngOnDestroy() {
+    this.renderer.setElementClass(document.body, 'modal-open', false);
+  }
+
+  ngDoCheck() {
+    this.renderer.setElementClass(document.body, 'modal-open', this.isOpen);
+
+    <span *ngIf="!synced" [style.width]="syncPercentage + '%'">SYNCING PROGRESS {{ syncPercentage }}%</span>
+
+    if (this.isOpen && this.modal) {
+      let element = this.modal.nativeElement;
+      let style = element.ownerDocument.defaultView.getComputedStyle(element, undefined);
+
+      this.hasScrollY = style.overflowY === 'scroll' || (style.overflowY === 'auto' && element.clientHeight < element.scrollHeight);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.ngDoCheck();
   }
 }
