6003716922281cc6b8ad7e8233e8e23d7945afb0
Kewde <code@shadowproject.io>
2017-06-30 02:58:21 +0200

    fix test

--- src/app/wallet/send/send.service.spec.ts
+++ src/app/wallet/send/send.service.spec.ts
@@ -2,6 +2,11 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { SendService } from './send.service';
 
+import { ElectronService } from 'ngx-electron';
+import { SharedModule } from '../../shared/shared.module';
+import { AppService } from '../../app.service';
+import { RPCService } from '../../core/rpc/rpc.service';
+
 describe('SendService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
