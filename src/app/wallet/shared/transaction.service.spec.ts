9ecb6acc7b69c1bf06828ae794a9fc2dfbf6bbaa
Kewde <code@shadowproject.io>
2017-07-03 03:44:11 +0200

    fix more karma tests

--- src/app/wallet/shared/transaction.service.spec.ts
+++ src/app/wallet/shared/transaction.service.spec.ts
@@ -2,10 +2,19 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { TransactionService } from './transaction.service';
 
+import { ElectronService } from 'ngx-electron';
+import { AppService } from '../../app.service';
+import { RPCService } from '../../core/rpc/rpc.service';
+
 describe('TransactionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TransactionService]
+      providers: [
+      	TransactionService,
+      	AppService,
+        ElectronService,
+        RPCService
+      ]
     });
   });
 
