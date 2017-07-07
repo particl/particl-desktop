5b72cbb186adcfc2f0cca2bc954bb45aa1ced4ed
Kewde <code@shadowproject.io>
2017-07-07 02:37:22 +0200

    fix karma

--- src/app/modals/passphrase/passphrase.component.spec.ts
+++ src/app/modals/passphrase/passphrase.component.spec.ts
@@ -3,13 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PassphraseComponent } from './passphrase.component';
 import { ModalsModule } from '../modals.module';
 
+import { ElectronService } from 'ngx-electron';
+import { AppService } from '../../app.service';
+import { RPCService } from '../../core/rpc/rpc.service';
+
+
 describe('PassphraseComponent', () => {
   let component: PassphraseComponent;
   let fixture: ComponentFixture<PassphraseComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ModalsModule]
+      imports: [ModalsModule],
+      providers: [
+        AppService,
+        ElectronService,
+        RPCService
+      ]
     })
     .compileComponents();
   }));
