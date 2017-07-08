fb7d6a712f782510c8b2e1a862a6b24f1362d325
Kewde <code@shadowproject.io>
2017-07-08 02:37:34 +0200

    Implement recovery phrase RPC

--- src/app/modals/recoverwallet/recoverwallet.component.ts
+++ src/app/modals/recoverwallet/recoverwallet.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AppService } from '../../app.service';
+
 @Component({
   selector: 'app-recoverwallet',
   templateUrl: './recoverwallet.component.html',
@@ -9,8 +11,34 @@ export class RecoverwalletComponent {
 
   words: string[] = Array(24).fill('');
 
+  constructor (private appService: AppService) { }
+
   restore(password: string) {
     // TODO API call
-    console.log(this.words, password);
+    let wordsString: string = '';
+    for(var i in this.words) {
+      const word = this.words[i];
+      if(word !== undefined && word !== '') {
+      	wordsString += ( (wordsString === '' ? '' : ' ') + word);
+      }
+    }
+    const params : Array<any> = this.getParams(wordsString, password);
+    this.appService.rpc.call(this, 'extkeygenesisimport', params, this.rpc_importFinished);
+    this.words = Array(24).fill('');
+  }
+
+  getParams(words, password): Array<any> {
+  	if(password === undefined) {
+  	  return [words];
+  	} else {
+  	  return [words, password];
+  	}
+  }
+
+  rpc_importFinished(JSON: Object) {
+  	// TODO for rpc service: needs ERROR handling!
+  	if(JSON['result'] === 'Success.') {
+  		alert('Importing the recovery phrase succeeded!');
+  	}
   }
 }
