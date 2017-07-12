88fed5c21d775d0c5c04faae996c0180a98a14a6
Kewde <code@shadowproject.io>
2017-07-12 13:21:30 +0200

    add RpcModule remove confirpassphrase

--- src/app/core/status/status.component.spec.ts
+++ src/app/core/status/status.component.spec.ts
@@ -3,10 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StatusComponent } from './status.component';
 
 import { SharedModule } from '../../shared/shared.module';
-import { ElectronService } from 'ngx-electron';
-
-import { PeerService } from '../rpc/peer.service';
-import { RPCService } from '../rpc/rpc.service';
+import { RpcModule } from './rpc.module';
 
 describe('StatusComponent', () => {
   let component: StatusComponent;
@@ -15,11 +12,9 @@ describe('StatusComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ StatusComponent ],
-      imports: [SharedModule],
-      providers: [
-        ElectronService,
-        RPCService,
-        PeerService
+      imports: [
+       SharedModule,
+       RpcModule.forRoot()
       ]
     })
     .compileComponents();
