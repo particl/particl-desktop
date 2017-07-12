d03a9d20fc6c4440591e291697eea8e4cc7da37d
Kewde <code@shadowproject.io>
2017-07-12 20:54:01 +0200

    add unlockwallet html

--- src/app/modals/unlockwallet/unlockwallet.component.ts
+++ src/app/modals/unlockwallet/unlockwallet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalsModule } from '../modals.module';
 
 import { RPCService } from '../../core/rpc/rpc.service';
@@ -9,6 +9,8 @@ import { RPCService } from '../../core/rpc/rpc.service';
   styleUrls: ['./unlockwallet.component.scss']
 })
 export class UnlockwalletComponent {
+
+  @Input() openUnlockWalletModal: boolean = false;
 
   constructor (private _rpc: RPCService) { }
 
