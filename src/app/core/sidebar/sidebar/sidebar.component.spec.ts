b2071871a92d6f9f09c523e76c1709f25b6aeede
Jason Eybers <jasoneybers@gmail.com>
2017-07-13 13:02:28 +0200

    Add compdoc and sidebar test

--- src/app/core/sidebar/sidebar/sidebar.component.spec.ts
+++ src/app/core/sidebar/sidebar/sidebar.component.spec.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
@@ -9,7 +11,10 @@ describe('SidebarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ],
+      declarations: [
+        TestCmpWrapper,
+        SidebarComponent
+      ],
       providers: [ SidebarService ]
     })
     .compileComponents();
@@ -24,4 +29,26 @@ describe('SidebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open and close', () => {
+    component.toggleOpen(false);
+    expect(component.isOpen).toBeFalsy();
+    component.toggleOpen(true); // TODO: THIS IS BROKEN, I think...
+    expect(component.isOpen).toBeFalsy();
+  });
+
+  it('should dock and undock', () => {
+    component.toggleDocked(false);
+    expect(component.isDocked).toBeFalsy();
+    component.toggleDocked(true); // TODO: THIS IS BROKEN, I think...
+    expect(component.isDocked).toBeFalsy();
+  });
+
 });
+
+@Component({
+ selector  : 'test-cmp',
+ template  : '<sidebar [position]="left"></sidebar>',
+})
+class TestCmpWrapper {
+}
