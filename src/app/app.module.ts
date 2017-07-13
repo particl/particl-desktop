42e34ae6deba3fe30c7642c9cae60f764bd7af33
Kewde <code@shadowproject.io>
2017-07-13 03:05:52 +0200

    trigger karma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
     SidebarModule.forRoot(),
     WalletModule.forRoot(),
     RpcModule.forRoot(),
-    ModalsModule
+    ModalsModule.forRoot()
   ],
   providers: [
     WindowService
