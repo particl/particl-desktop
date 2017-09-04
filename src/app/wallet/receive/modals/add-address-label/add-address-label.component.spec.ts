f07ab43734a4955b501c1ca5d4c73b7377630982
Kirti Anand <kirti.a@cisinlabs.com>
2017-09-04 22:16:56 +0530

    Creates AddAddressLable component

---
+++ src/app/wallet/receive/modals/add-address-label/add-address-label.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddAddressLabelComponent } from './add-address-label.component';
+
+describe('AddAddressLabelComponent', () => {
+  let component: AddAddressLabelComponent;
+  let fixture: ComponentFixture<AddAddressLabelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddAddressLabelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddAddressLabelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/wallet/receive/modals/add-address-label/add-address-label.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RPCService } from '../../../../core/rpc/rpc.service';
+import { MdDialogRef } from '@angular/material';
+import { FlashNotificationService } from '../../../../services/flash-notification.service';
+
+@Component({
+  selector: 'app-add-address-label',
+  templateUrl: './add-address-label.component.html',
+  styleUrls: ['./add-address-label.component.scss']
+})
+export class AddAddressLabelComponent implements OnInit {
+
+  @Output() onAddressAdd = new EventEmitter();
+
+  public addLableForm: FormGroup;
+  public type: string;
+  public label: string;
+
+  constructor(
+    public dialogRef: MdDialogRef<AddAddressLabelComponent>,
+    private formBuilder: FormBuilder,
+    private rpc: RPCService,
+    private flashNotificationService : FlashNotificationService
+  ) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.addLableForm = this.formBuilder.group({
+      label: this.formBuilder.control(null, [Validators.required]),
+    });
+  }
+
+  onSubmitForm(): void {
+    const call = this.type === 'public' ? 'getnewaddress' : (this.type === 'private' ? 'getnewstealthaddress' : '');
+
+    if (!!call) {
+      this.rpc.call(call, [this.label])
+        .subscribe(response => {
+            this.onAddressAdd.emit(response);
+            this.dialogRef.close();
+            this.flashNotificationService.open(`New ${this.type} address lable added !!`)
+          });
+    }
+  }
+
+}
