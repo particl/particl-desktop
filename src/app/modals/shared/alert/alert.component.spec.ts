9ad72f5e671854f1b5718b122b18c69efcbcd0e8
Kewde <code@shadowproject.io>
2017-09-12 18:18:04 +0200

    Import ModalsModule in test

--- src/app/modals/shared/alert/alert.component.spec.ts
+++ src/app/modals/shared/alert/alert.component.spec.ts
@@ -2,13 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
 
+import { ModalsModule } from '../../modals.module';
+
+import { SharedModule } from '../../../shared/shared.module';
+import { RpcModule } from '../../../core/rpc/rpc.module';
+
+
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertComponent ]
+      declarations: [ AlertComponent ],
+      imports: [
+        SharedModule,
+        RpcModule.forRoot(),
+        ModalsModule
+      ]
     })
     .compileComponents();
   }));
