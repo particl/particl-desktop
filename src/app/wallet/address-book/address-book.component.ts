196fcd7980ccd1e35708981b1c8e17289f99a1d3
Kirti Anand <kirti.a@cisinlabs.com>
2017-09-12 22:59:11 +0530

    Remove unwanted code and finalize add edit and delete address functionalty with modal

--- src/app/wallet/address-book/address-book.component.ts
+++ src/app/wallet/address-book/address-book.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { MdDialog } from '@angular/material';
 import { NewAddressModalComponent } from './modal/new-address-modal/new-address-modal.component';
+import { Log } from 'ng2-logger';
 
 @Component({
   selector: 'app-address-book',
@@ -9,11 +10,19 @@ import { NewAddressModalComponent } from './modal/new-address-modal/new-address-
 })
 export class AddressBookComponent {
 
+  log: any = Log.create('address-book.component');
   public query: string;
   constructor(private dialog: MdDialog) {
   }
 
   openNewAddress(): void {
     const dialogRef = this.dialog.open(NewAddressModalComponent);
+  }
+
+  editLabel(address: string): void {
+    this.log.d(`editLabel, address: ${address}`);
+    const dialogRef = this.dialog.open(NewAddressModalComponent);
+    dialogRef.componentInstance.address = address;
+    dialogRef.componentInstance.isEdit = true;
   }
 }
