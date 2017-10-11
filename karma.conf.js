9691689a9d8054d389b133fbdacbcf4d671ceece
Kirti Anand <kirti.a@cisinlabs.com>
2017-10-11 13:09:30 +0530

    Remove material css from karma config

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false },
-      'node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css'
+      { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
