5964ded513373591980e7db7a502a4463e5fa1d0
Kirti Anand <kirti.a@cisinlabs.com>
2017-10-16 19:21:14 +0530

    Add unlock model while new private address add

--- src/app/wallet/receive/modals/add-address-label/add-address-label.component.ts
+++ src/app/wallet/receive/modals/add-address-label/add-address-label.component.ts
@@ -1,8 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RPCService } from '../../../../core/rpc/rpc.service';
-import { MdDialogRef } from '@angular/material';
+import {MdDialog, MdDialogRef} from '@angular/material';
 import { FlashNotificationService } from '../../../../services/flash-notification.service';
+import { Log } from 'ng2-logger';
+import {ModalsComponent} from "../../../../modals/modals.component";
+import {ModalsService} from "../../../../modals/modals.service";
 
 @Component({
   selector: 'app-add-address-label',
@@ -16,12 +19,15 @@ export class AddAddressLabelComponent implements OnInit {
   public addLableForm: FormGroup;
   public type: string;
   public label: string;
+  log: any = Log.create('receive.component');
 
   constructor(
     public dialogRef: MdDialogRef<AddAddressLabelComponent>,
     private formBuilder: FormBuilder,
     private rpc: RPCService,
-    private flashNotificationService: FlashNotificationService
+    private flashNotificationService: FlashNotificationService,
+    private dialog: MdDialog,
+    private _modals: ModalsService
   ) { }
 
   ngOnInit() {
@@ -35,15 +41,27 @@ export class AddAddressLabelComponent implements OnInit {
   }
 
   onSubmitForm(): void {
-    const call = (this.type === 'public' ? 'getnewaddress' : (this.type === 'private' ? 'getnewstealthaddress' : ''));
+    if (['Locked', 'Unlocked, staking only'].indexOf(this.rpc.state.get('encryptionstatus')) !== -1) {
+      // unlock wallet and send transaction
+      this.dialog.open(ModalsComponent, {disableClose: true, width: '100%', height: '100%'});
+      this._modals.open('unlock', {forceOpen: true, timeout: 3, callback: this.addNewLabel.bind(this)});
+    } else {
+      // wallet already unlocked
+      this.addNewLabel();
+    }
+  }
 
+  addNewLabel(): void{
+    const call = (this.type === 'public' ? 'getnewaddress' : (this.type === 'private' ? 'getnewstealthaddress' : ''));
+    this.log.d(call, 'newAddress: successfully retrieved new address');
     if (!!call) {
       this.rpc.call(call, [this.label])
         .subscribe(response => {
-            this.onAddressAdd.emit(response);
-            this.dialogRef.close();
-            this.flashNotificationService.open(`New ${this.type} address lable added !!`)
-          });
+          this.log.d(call, 'newAddress: successfully retrieved new address');
+          this.onAddressAdd.emit(response);
+          this.dialogRef.close();
+          this.flashNotificationService.open(`New ${this.type} address lable added !!`)
+        });
     }
   }
 
