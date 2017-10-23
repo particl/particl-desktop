c675529aac5686077e24d65efda14e58f5a028d0
Kewde <code@shadowproject.io>
2017-10-23 21:58:43 +0200

    fix test

--- src/app/overview/overview.component.spec.ts
+++ src/app/overview/overview.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OverviewComponent } from './overview.component';
 import { StakinginfoComponent } from './widgets/stakinginfo/stakinginfo.component';
+import { ColdstakeComponent } from './widgets/coldstake/coldstake.component';
 import { SharedModule } from '../shared/shared.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { RpcModule } from '../core/rpc/rpc.module';
