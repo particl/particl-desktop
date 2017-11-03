9451396273d8f93f0923eaa3c3d96b7580356ad0
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-11-03 16:38:33 +0100

    typo

--- modules/rpc/cookie.js
+++ modules/rpc/cookie.js
@@ -101,7 +101,7 @@ function getAuth(options) {
 
   let auth;
   const COOKIE_FILE = findCookiePath()
-                    + options.testnet ? '/testnet' : '' + '/.cookie');
+                    + options.testnet ? '/testnet' : '' + '/.cookie';
 
   if (fs.existsSync(COOKIE_FILE)) {
     auth = fs.readFileSync(COOKIE_FILE, 'utf8').trim();
