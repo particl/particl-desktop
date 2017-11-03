7a9a60a6ec32435f07375534d8efa7eb7a0e9225
Kirti Anand <kirti.a@cisinlabs.com>
2017-11-03 23:27:15 +0530

    Refactor sign/verify message

--- src/app/wallet/shared/signature-address-modal/signature-address-modal.component.spec.ts
+++ src/app/wallet/shared/signature-address-modal/signature-address-modal.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignatureAddressModalComponent } from './signature-address-modal.component';
+import { WalletModule } from '../../wallet.module';
+import { RpcModule } from '../../../core/rpc/rpc.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { FlashNotificationService } from '../../../services/flash-notification.service';
+import { MdSnackBarModule } from '@angular/material';
+import { ModalsService } from '../../../modals/modals.service';
 
 describe('SignatureAddressModalComponent', () => {
   let component: SignatureAddressModalComponent;
@@ -8,9 +14,15 @@ describe('SignatureAddressModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignatureAddressModalComponent ]
+      imports: [
+        SharedModule,
+        WalletModule,
+        RpcModule.forRoot(),
+        MdSnackBarModule
+      ],
+      providers: [FlashNotificationService, ModalsService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
