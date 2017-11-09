3df3ac995d7e42ff4cf8fe53ea2bed8d9b53477e
vikas <vikas.v@cisinlabs.com>
2017-11-09 14:51:05 +0530

    tweak in paste functionality

--- src/app/modals/createwallet/passphrase/passphrase.component.ts
+++ src/app/modals/createwallet/passphrase/passphrase.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,  OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, Input,  OnChanges, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 import { ClipboardModule } from 'ngx-clipboard';
 
 import { PassphraseService } from './passphrase.service';
@@ -20,8 +20,9 @@ export class PassphraseComponent implements  OnChanges {
   focused: number = 0;
   public editable: number[] = [];
 
-  @Input() words: string[] = Array(MAX_WORDS).fill('');
+  @ViewChild('phrase') phrase: ElementRef;
 
+  @Input() words: string[] = Array(MAX_WORDS).fill('');
   @Input() readOnly: boolean = false;
   @Input() isDisabled: boolean = false;
   @Input() partialDisable: boolean = false;
@@ -48,7 +49,6 @@ export class PassphraseComponent implements  OnChanges {
   }
 
   splitAndFill(index: number): void {
-
     if (this.partialDisable || this.words[index].indexOf(' ') === -1) {
       return;
     }
@@ -92,7 +92,7 @@ export class PassphraseComponent implements  OnChanges {
 
   pasteContent() {
     // Directive doesn't get reflect if we make focused = 0
-    document.getElementById('text-input-' + this.focused).focus();
+    this.phrase.nativeElement.focus();
     document.execCommand('Paste');
   }
 }
