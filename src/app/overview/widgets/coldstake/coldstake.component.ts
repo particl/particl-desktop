b09f6aa634336de4f408e5aec7547832508aa792
Kewde <code@shadowproject.io>
2017-11-09 16:22:01 +0000

    Add progress bar to coldstake widget (#290)
    * add coldstake progress bar
    
    * fix test
    
    * Update coldstake.component.ts
    
    * Check if wallet is locked before assuming
    
    * remove additional lock code
    
    * only execute when coldstaking is enabled
    
    * fix lint
    
    * Cold staking: copy tweaks + added case for 100% progress done
    
    * fix lint

--- src/app/overview/widgets/coldstake/coldstake.component.ts
+++ src/app/overview/widgets/coldstake/coldstake.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Log } from 'ng2-logger';
 
 import { RPCService } from '../../../core/rpc/rpc.service';
 import { ModalsService } from '../../../modals/modals.service';
+
+import { Amount } from '../../../shared/util/utils';
 
 @Component({
   selector: 'app-coldstake',
@@ -10,20 +13,53 @@ import { ModalsService } from '../../../modals/modals.service';
 })
 export class ColdstakeComponent implements OnInit {
 
-  private coldStakingEnabled: boolean = undefined;
+  /*  General   */
+  private log: any = Log.create('coldstake.component');
+  coldStakingEnabled: boolean = undefined;
+
+  private progress: Amount = new Amount(0, 2);
+  get coldstakeProgress(): number { return this.progress.getAmount() }
 
   constructor(
     private _modals: ModalsService,
     private _rpc: RPCService
   ) {
-    this._rpc.state.observe('ui:coldstaking').subscribe(status => this.coldStakingEnabled = status);
+    this._rpc.state.observe('ui:coldstaking')
+    .subscribe(status => this.coldStakingEnabled = status);
+
+    this.rpc_progressLoop();
   }
 
   ngOnInit() {
   }
 
-  isColdStakingEnabled() {
-    return this.coldStakingEnabled;
+  /** calls listunspent, then calculate progress. */
+  private rpc_progressLoop() {
+
+    if (this.coldStakingEnabled) {
+      this._rpc.call('listunspent')
+        .subscribe(
+          (response: Array<any>) => {
+            let activeCount = 0;
+            let totalCount = 0;
+
+            response.forEach((output) => {
+              totalCount += output.amount;
+
+              if (output.coldstaking_address !== undefined) {
+                activeCount += output.amount;
+              }
+              this.log.d(`activeCount=${activeCount} totalCount=${totalCount}`);
+              this.progress = new Amount((activeCount / totalCount) * 100, 2);
+           });
+         },
+      // TODO: Handle error appropriately
+      error => this.log.er('rpc_progressLoop: listunspent failed', error));
+    }
+
+    if (this.coldstakeProgress < 100) {
+      setTimeout(this.rpc_progressLoop.bind(this), 1000);
+    }
   }
 
   openUnlockWalletModal() {
