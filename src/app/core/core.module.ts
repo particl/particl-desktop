2e0254520f41d81f5f4e5750ce22f1f3969a4504
Kewde <code@shadowproject.io>
2017-11-17 14:17:58 +0100

    add http to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
-import { IpcService } from './ipc/ipc.service';
 
 import { RpcModule } from './rpc/rpc.module';
 
+import { IpcService } from './ipc/ipc.service';
 import { RpcService } from './rpc/rpc.service';
 import { BlockStatusService } from './rpc/blockstatus/blockstatus.service'
 import { PeerService } from './rpc/peer/peer.service';
@@ -19,6 +20,9 @@ import { WindowService } from './window/window.service';
   imports: [
     CommonModule,
     RpcModule.forRoot() // TODO: should be here?
+  ],
+  exports: [
+    HttpModule
   ],
   declarations: []
 })
