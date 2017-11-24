ef5c099b9b0aafdbc730bcd7f736734220f88d83
Kewde <code@shadowproject.io>
2017-11-24 04:14:12 +0100

    ipc

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,6 @@ import { RpcModule } from './rpc/rpc.module';
 import { IpcService } from './ipc/ipc.service';
 import { RpcService } from './rpc/rpc.service';
 import { BlockStatusService } from './rpc/blockstatus/blockstatus.service'
-import { IpcService } from './ipc/ipc.service';
 import { PeerService } from './rpc/peer/peer.service';
 import { StateService } from './state/state.service';
 import { WindowService } from './window/window.service';
@@ -42,9 +41,10 @@ export class CoreModule {
 
 export { WindowService } from './window/window.service';
 
+export { IpcService } from './ipc/ipc.service';
 export { RpcService } from './rpc/rpc.service';
 export { BlockStatusService } from './rpc/blockstatus/blockstatus.service'
 export { PeerService } from './rpc/peer/peer.service';
 export { SnackbarService } from './snackbar/snackbar.service';
 export { StateService } from './state/state.service';
-export { IpcService } from './ipc/ipc.service';
+
