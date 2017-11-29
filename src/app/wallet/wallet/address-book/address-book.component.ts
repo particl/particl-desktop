9ba6c9527b715533a586b513b8a4aae1d71d5baf
Unknown <jasonheybers@gmail.com>
2017-11-29 16:32:41 +0200

    implemented error on paste
    if the address that is pasted is not valid, flash error will be triggered with an appropriate error message

--- src/app/wallet/wallet/address-book/address-book.component.ts
+++ src/app/wallet/wallet/address-book/address-book.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material';
 import { Log } from 'ng2-logger';
 
 import { NewAddressModalComponent } from './modal/new-address-modal/new-address-modal.component';
+import { SnackbarService } from '../../../core/snackbar/snackbar.service';
+
 
 @Component({
   selector: 'app-address-book',
@@ -13,7 +15,8 @@ export class AddressBookComponent {
 
   log: any = Log.create('address-book.component');
   public query: string;
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog,
+              private flashNotificationService: SnackbarService) {
   }
 
   openNewAddress(): void {
@@ -28,12 +31,13 @@ export class AddressBookComponent {
   }
 
   @HostListener('document:paste', ['$event'])
-  onPaste(event) {
+  onPaste(event: any) {
     const address = event.clipboardData.getData('text');
     if (/^[pPrRTt][a-km-zA-HJ-NP-Z1-9]{25,}$/.test(address)) {
       if (!this.dialog.openDialogs.length) {
         this.editLabel(address);
         this.dialog.openDialogs[0].componentInstance.isEdit = false;
+        this.flashNotificationService.open('This is your own address - can not be added to Address book!');
       }
     }
   }
