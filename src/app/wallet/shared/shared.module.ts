5bc0c5d17e2d0f51199378fb96eb5413e03fc0ea
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-12-13 15:13:58 +0100

    layout

--- src/app/wallet/shared/shared.module.ts
+++ src/app/wallet/shared/shared.module.ts
@@ -2,29 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ClipboardModule } from 'ngx-clipboard';
-
 import { AccordionModule } from './accordion/accordion.module';
 
 import { HeaderComponent } from './header/header.component';
 import { TableComponent } from './table/table.component';
 import { GridComponent } from './grid/grid.component';
-import { MatButtonModule, MatDialogModule, MatExpansionModule, MatIconModule, MatInputModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DeleteConfirmationModalComponent } from './delete-confirmation-modal/delete-confirmation-modal.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     AccordionModule,
+    MatButtonModule,
     MatDialogModule,
     MatExpansionModule,
-    MatInputModule,
     MatIconModule,
-    // BrowserAnimationsModule,
-    MatButtonModule,
+    MatInputModule,
     FlexLayoutModule
   ],
   declarations: [
