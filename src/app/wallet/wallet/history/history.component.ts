147d463b75176ebfa856190338a279d7514b7677
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-12-13 16:38:35 +0100

    functionnal controls

--- src/app/wallet/wallet/history/history.component.ts
+++ src/app/wallet/wallet/history/history.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { TransactionCategory } from '../shared/transaction.model';
 
@@ -8,32 +8,39 @@ import { TransactionCategory } from '../shared/transaction.model';
   styleUrls: ['./history.component.scss']
 })
 
-export class HistoryComponent {
-
-  narration: string = 'Both';
-  filter:    string;
-
-  // TODO : key-value
-  narrationChoices: Array<string> = ['With narration', 'Without narration', 'Both'];
-  filterOptions:    Array<string> = [
-    'Received',
-    'Sent',
-    'Staked',
-    'Orphaned stakes',
-    'Balance transfer'
-  ];
+export class HistoryComponent implements OnInit {
 
   @ViewChild('filterList') filterList: any;
 
+  narrationChoices: Array<any> = [
+    { title: 'With narration',    value: 'narrationOnly'    },
+    { title: 'Without narration', value: 'noNarration'      },
+    { title: 'Both',              value: 'any'              }
+  ];
+  filterOptions:    Array<any> = [
+    { title: 'Received',          value: "received"         },
+    { title: 'Sent',              value: "sent"             },
+    { title: 'Staked',            value: "stake"            },
+    { title: 'Orphaned stakes',   value: "orphaned_stake"   },
+    { title: 'Balance transfer',  value: "balance_transfer" }
+  ];
+
+  narration: string = 'any';
+  filters: Array<string> = [];
+
   constructor() { }
 
-  dump() {
-    console.log(this.narration, this.filter);
+  ngOnInit() {
+    this.filterList.selectedOptions.onChange.subscribe(item => {
+      item.added.map(added => this.filters.push(added.value));
+      item.removed.map(removed => this.filters = this.filters.filter((val: any) => {
+        return val !== removed.value;
+      }));
+    });
   }
 
   clear() {
-    console.log("clear");
-    this.narration = 'Both';
+    this.narration = 'any';
     this.filterList.deselectAll();
   }
 }
