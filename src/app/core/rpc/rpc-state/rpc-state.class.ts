20b506c83af65f7caf401fa4196f6c7bad231006
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2017-12-21 22:09:24 +0100

    indentation

--- src/app/core/rpc/rpc-state/rpc-state.class.ts
+++ src/app/core/rpc/rpc-state/rpc-state.class.ts
@@ -22,17 +22,17 @@ export class RpcStateClass {
 
   private lastBlockTimeState() {
     let _checkLastBlock = false;
-    this._rpc.state.observe('mediantime').subscribe(
-      mediantime => {
-        const lastblocktime = new Date(mediantime * 1000);
-        if (!_checkLastBlock && new Date().getTime() - (4 * 60 * 1000) > lastblocktime.getTime()) {
-          setTimeout(() => {
-            _checkLastBlock = false;
-            this._rpc.stateCall('getblockchaininfo');
-          }, 100);
-          _checkLastBlock = true;
-        }
-      });
+    this._rpc.state.observe('mediantime').subscribe(mediantime => {
+      const now = new Date().getTime() - (4 * 60 * 1000);
+      const lastblocktime = new Date(mediantime * 1000);
+      if (!_checkLastBlock && now > lastblocktime.getTime()) {
+        _checkLastBlock = true;
+        setTimeout(() => {
+          _checkLastBlock = false;
+          this._rpc.stateCall('getblockchaininfo');
+        }, 100);
+      }
+    });
   }
 
   private blockLoop() {
@@ -44,8 +44,8 @@ export class RpcStateClass {
 
   private walletLockedState() {
     this._rpc.state.observe('encryptionstatus')
-    .subscribe(status => this._rpc.state
-      .set('locked', ['Locked', 'Unlocked, staking only'].includes(status)));
+      .subscribe(status => this._rpc.state
+        .set('locked', ['Locked', 'Unlocked, staking only'].includes(status)));
   }
 
   /*
@@ -54,45 +54,41 @@ export class RpcStateClass {
   *   update the coldstaking state.
   */
   private coldStakeHook() {
-    this._rpc.state.observe('locked').subscribe(
-      locked => {
-        if (locked === false) {
-          // only available if unlocked
-          this._rpc.call('walletsettings', ['changeaddress'])
-          .subscribe(
-            // set state for coldstaking
-            response => this._rpc.state.set('ui:coldstaking',
-              response.changeaddress === 'default' ? false : !!response.changeaddress.coldstakingaddress
-              ),
-            error => this.log.er('walletsettings changeaddress, returned an error', error));
-
-        }
-      });
+    this._rpc.state.observe('locked').subscribe(locked => {
+      if (locked === false) {
+        // only available if unlocked
+        this._rpc.call('walletsettings', ['changeaddress'])
+        .subscribe(
+          // set state for coldstaking
+          response => this._rpc.state.set('ui:coldstaking',
+            response.changeaddress === 'default'
+              ? false
+              : !!response.changeaddress.coldstakingaddress
+          ),
+          error => this.log.er('walletsettings changeaddress', error)
+        );
+      }
+    });
   }
 
   private initWalletState() {
 
-    this._rpc.state.observe('encryptionstatus').take(1)
-    .subscribe(
-      status => {
-        const locked = this._rpc.state.get('locked');
+    this._rpc.state.observe('encryptionstatus').take(1).subscribe(status => {
+      const locked = this._rpc.state.get('locked');
 
-        if (locked) {
+      if (locked) {
+        this._rpc.state.set('ui:walletInitialized', true);
+        return;
+      }
+
+      this._rpc.call('extkey', ['list']).subscribe(response => {
+        // check if account is active
+        if (response.result === 'No keys to list.') {
+          this._rpc.state.set('ui:walletInitialized', false);
+        } else {
           this._rpc.state.set('ui:walletInitialized', true);
-          return;
         }
-
-        this._rpc.call('extkey', ['list'])
-        .subscribe(
-          response => {
-                // check if account is active
-                if (response.result === 'No keys to list.') {
-                  this._rpc.state.set('ui:walletInitialized', false);
-                } else {
-                  this._rpc.state.set('ui:walletInitialized', true);
-                }
-              },
-              error => this.log.er('RPC Call returned an error', error));
-      });
+      }, error => this.log.er('RPC Call returned an error', error));
+    });
   }
 }
