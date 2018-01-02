aa6d98961e51b72f98079a54deadb1a3b7f2123b
vikas <vikas.v@cisinlabs.com>
2018-01-02 21:55:35 +0530

    small fix

--- src/app/core-ui/main/status/status.component.ts
+++ src/app/core-ui/main/status/status.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
 import { Log } from 'ng2-logger';
 
