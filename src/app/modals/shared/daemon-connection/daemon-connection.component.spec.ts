991d0b828ad71ba13abe2cb270fb2dc98ebaf6b5
vikas <vikas.v@cisinlabs.com>
2018-01-15 15:48:05 +0530

    test case fix

--- src/app/modals/shared/daemon-connection/daemon-connection.component.spec.ts
+++ src/app/modals/shared/daemon-connection/daemon-connection.component.spec.ts
@@ -1,4 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material';
+
+import { ModalsModule } from '../../modals.module';
+import { CoreModule } from '../../../core/core.module';
+import { SharedModule } from '../../../wallet/shared/shared.module';
+import { CoreUiModule } from '../../../core-ui/core-ui.module';
 
 import { DaemonConnectionComponent } from './daemon-connection.component';
 
@@ -8,7 +14,15 @@ describe('DaemonConnectionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DaemonConnectionComponent ]
+      imports: [
+        SharedModule,
+        CoreModule.forRoot(),
+        ModalsModule,
+        CoreUiModule.forRoot()
+      ],
+      providers: [
+        { provide: MatDialogRef }
+      ]
     })
     .compileComponents();
   }));
