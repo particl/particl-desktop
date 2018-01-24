c55c6538c451fadf8bb54c80d5d5962a87bf8d9a
Martin Allien <martin@allienworks.net>
2018-01-24 18:01:33 +0100

    modal layout debug (WIP)

--- src/app/modals/modals.service.ts
+++ src/app/modals/modals.service.ts
@@ -79,7 +79,7 @@ export class ModalsService implements OnDestroy {
     * @param {any} data       Optional - data to pass through to the modal.
     */
   open(modal: string, data?: any): void {
-    const dialogRef = this._dialog.open(ModalsComponent, {disableClose: true, width: '100%', height: '100%'});
+    const dialogRef = this._dialog.open(ModalsComponent, {disableClose: true});
     if (modal in this.messages) {
       if (
         (data && data.forceOpen)
