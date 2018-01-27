e7ff44ea3d3c0bf3df3605b34abf679b79af2c7c
kewde <code@shadowproject.io>
2018-01-27 21:36:48 +0100

    window & ipc interface into typings

--- src/app/core/ipc/ipc.service.ts
+++ src/app/core/ipc/ipc.service.ts
@@ -5,23 +5,7 @@ import { Log } from 'ng2-logger'
 import { IpcListener, ObservableFactoryFunction, Receiver, ListenerEvent } from './ipc.types';
 
 // RxIPC related stuffs
-// TODO: maybe move into ipc.types ?
-declare global {
-  interface Window {
-    electron: boolean;
-    require: any;
-    ipc: {
-      on: (channel: string, listener: Function) => void;
-      once: (channel: string, listener: Function) => void;
-      send: (channel: string, arguments?: {}) => void;
-      sendSync: (channel: string, arguments?: {}) => void;
-      sendToHost: (channel: string, arguments?: {}) => void;
-      removeListener: (channel: string, listener: Function) => void;
-      removeAllListeners: (channel?: string) => void;
-      listenerCount: (channel?: string) => number;
-    }
-  }
-}
+
 
 @Injectable()
 export class IpcService {
