c0960ed55cb9e0611e61b3b26f14bf36de87c4d8
kewde <code@shadowproject.io>
2018-02-01 15:05:20 +0100

    bit more advanced

--- src/app/market/overview-listings/preview-listing/preview-listing.component.ts
+++ src/app/market/overview-listings/preview-listing/preview-listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-preview-listing',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-listing.component.scss']
 })
 export class PreviewListingComponent implements OnInit {
+
+  @Input() listing: any;
 
   constructor() { }
 
