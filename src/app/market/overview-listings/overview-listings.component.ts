be81dfab542c7fc81a5f142932404020953932d5
Martin Allien <martin@allienworks.net>
2018-02-01 16:42:44 +0100

    Listings: control bar basics

--- src/app/market/overview-listings/overview-listings.component.ts
+++ src/app/market/overview-listings/overview-listings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-overview-listings',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview-listings.component.scss']
 })
 export class OverviewListingsComponent implements OnInit {
+
+  // filters
+  countries = new FormControl();
+  countryList = ['Europe', 'North America', 'South America', 'Asia', 'Africa', 'Moon'];
+
+  // TODO? "Select with option groups" - https://material.angular.io/components/select/overview#creating-groups-of-options
+  categories = new FormControl();
+  categoryList = ['Electronics', 'Hobby', 'Health & Beauty', 'Toys', 'Gardening', 'Food', 'Digital', 'Whatever else'];
 
   listings: Array<any> = [
     "1",
