85dcce02bcdee4431197df43a7d0b923f9653558
kewde <code@shadowproject.io>
2018-02-03 14:15:01 +0100

    fix tests

--- src/app/wallet/main/main-view.component.spec.ts
+++ src/app/wallet/main/main-view.component.spec.ts
@@ -1,15 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { MatDialogModule, MatDialogRef } from '@angular/material';
+import { MatDialog } from '@angular/material'; // TODO: move to material
 
 import { CoreModule } from '../../core/core.module';
 import { MainViewModule } from './main-view.module';
-// should be declared in module^
-import { MainViewComponent } from './main-view.component';
 import { ModalsModule } from '../../modals/modals.module';
 
-import { MatDialogModule, MatDialogRef } from '@angular/material';
-import { MatDialog } from '@angular/material'; // TODO: move to material
+// should be declared in module^
+import { MainViewComponent } from './main-view.component';
+
 
 describe('MainViewComponent', () => {
   let component: MainViewComponent;
