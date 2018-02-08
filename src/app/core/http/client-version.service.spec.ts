ece77bcde9d62f06f3b0de2bc30f4740f47e9bd9
Kirti Anand <kirti.a@cisinlabs.com>
2018-02-08 17:40:10 +0530

    Resolve test case

--- src/app/core/http/client-version.service.spec.ts
+++ src/app/core/http/client-version.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ClientVersionService } from './client-version.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ClientVersionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       providers: [ClientVersionService]
     });
   });
