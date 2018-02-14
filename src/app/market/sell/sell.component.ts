08a97f98550eec8c358a8efa433e8f6004805acb
Martin Allien <martin@allienworks.net>
2018-02-14 19:25:38 +0100

    Sell: Listings/Orders final layout

--- src/app/market/sell/sell.component.ts
+++ src/app/market/sell/sell.component.ts
@@ -31,15 +31,22 @@ export class SellComponent implements OnInit {
   ];
 
   // TODO: disable radios for 0 amount-statuses
-  statuses: Array<any> = [
-    { title: 'All orders', value: 'all',     amount: '5' },
-    { title: 'Pending',    value: 'pending', amount: '1' },
-    { title: 'Listed',     value: 'listed',  amount: '2' },
-    { title: 'Bidding',    value: 'bidding', amount: '0' },
+  listing_statuses: Array<any> = [
+    { title: 'All listings', value: 'all',         amount: '5' },
+    { title: 'Unpublished',  value: 'unpublished', amount: '1' }, // all unpublished = pending (?), sold & expired
+    { title: 'Pending',      value: 'pending',     amount: '1' },
+    { title: 'Listed',       value: 'listed',      amount: '2' },
+    { title: 'Sold',         value: 'sold',        amount: '1' },
+    { title: 'Expired',      value: 'expired',     amount: '0' },
+  ];
+
+  // TODO: disable radios for 0 amount-statuses
+  order_statuses: Array<any> = [
+    { title: 'All orders', value: 'all',     amount: '3' },
+    { title: 'Bidding',    value: 'bidding', amount: '1' },
     { title: 'In escrow',  value: 'escrow',  amount: '0' },
     { title: 'Shipped',    value: 'shipped', amount: '1' },
     { title: 'Sold',       value: 'sold',    amount: '1' },
-    { title: 'Expired',    value: 'expired', amount: '0' },
   ];
 
   filters: any = {
