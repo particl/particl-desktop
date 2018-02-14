cb562d343d69cd745e0b4841a8214842749411fc
Kirti Anand <kirti.a@cisinlabs.com>
2018-02-14 22:55:44 +0530

    Creates TransactionBuilder model

---
+++ src/app/wallet/wallet/send/transaction-builder.model.ts
@@ -0,0 +1,18 @@
+export class TransactionBuilder {
+  input: string;
+  output: string;
+  toAddress: string;
+  toLabel: string;
+  address: string;
+  amount: number;
+  comment: string;
+  narration: string;
+  numsignatures: number;
+  validAddress: boolean;
+  validAmount: boolean;
+  isMine: boolean;
+  currency: string;
+  privacy: number;
+  subtractFeeFromAmount: boolean;
+  note: string;
+}
