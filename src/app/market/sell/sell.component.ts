927811cdb2a0e3af05f4b49ac613982420684c46
Martin Allien <martin@allienworks.net>
2018-02-15 12:32:03 +0100

    Sell > Listings examples filled

--- src/app/market/sell/sell.component.ts
+++ src/app/market/sell/sell.component.ts
@@ -31,22 +31,65 @@ export class SellComponent implements OnInit {
   ];
 
   // TODO: disable radios for 0 amount-statuses
-  listing_statuses: Array<any> = [
+  listing_filtering: Array<any> = [
     { title: 'All listings', value: 'all',         amount: '5' },
     { title: 'Unpublished',  value: 'unpublished', amount: '1' }, // all unpublished = pending (?), sold & expired
     { title: 'Pending',      value: 'pending',     amount: '1' },
     { title: 'Listed',       value: 'listed',      amount: '2' },
     { title: 'Sold',         value: 'sold',        amount: '1' },
-    { title: 'Expired',      value: 'expired',     amount: '0' },
+    { title: 'Expired',      value: 'expired',     amount: '0' }
   ];
 
   // TODO: disable radios for 0 amount-statuses
-  order_statuses: Array<any> = [
+  order_filtering: Array<any> = [
     { title: 'All orders', value: 'all',     amount: '3' },
     { title: 'Bidding',    value: 'bidding', amount: '1' },
     { title: 'In escrow',  value: 'escrow',  amount: '0' },
     { title: 'Shipped',    value: 'shipped', amount: '1' },
-    { title: 'Sold',       value: 'sold',    amount: '1' },
+    { title: 'Sold',       value: 'sold',    amount: '1' }
+  ];
+
+  listings: Array<any> = [
+    {
+      name: 'My basic listing template',
+      category: 'Electronics, DIY',
+      status: 'unpublished',
+      status_info: 'Inactive, unpublished listing template – used to tweak your listing before publishing (or after you take down your active listings later)',
+      action_icon: 'part-check',
+      action_button: 'Publish',
+      action_tooltip: 'Activate listing and put it on sale',
+      action_color: 'primary'
+    },
+    {
+      name: 'Fresh product (2 kg)',
+      category: 'Food, Cosmetics',
+      status: 'pending',
+      status_info: 'Listing successfully created and paid, waiting to be published on the market (usually needs XX confirmations before going live)',
+      action_icon: 'part-date',
+      action_button: 'Waiting for publication', // TODO: disable this button
+      action_tooltip: 'Awaiting confirmations before making the listing live',
+      action_color: 'primary'
+    },
+    {
+      name: 'The most delicious Particl-branded cupcakes',
+      category: 'Munchies',
+      status: 'listed',
+      status_info: 'Active, published listing visible on the market – available for purchase',
+      action_icon: 'part-error',
+      action_button: 'Unpublish',
+      action_tooltip: 'Take the listing off market',
+      action_color: 'warn'
+    },
+    {
+      name: 'NFC-enabled contactless payment perfume',
+      category: 'Toys for girls',
+      status: 'expired',
+      status_info: 'Expired, inactive listing – can be relisted on the market again after payment of listing fee',
+      action_icon: 'part-check',
+      action_button: 'Publish again',
+      action_tooltip: 'Re-list expired listing on the market',
+      action_color: 'primary'
+    },
   ];
 
   filters: any = {
