3ea756bb33bd6258bc3a08a2c45c39e492ca3ff2
Kirti Anand <kirti.a@cisinlabs.com>
2018-02-15 15:29:29 +0530

    Remove clear function in sendComponent

--- src/app/wallet/wallet/send/send.component.ts
+++ src/app/wallet/wallet/send/send.component.ts
@@ -154,19 +154,6 @@ export class SendComponent {
         error => this.log.er('verifyAddress: validateAddressCB failed'));
   }
 
-  /** Clear the send object. */
-  clear(): void {
-    this.send = {
-      input: this.send.input,
-      output: this.send.output,
-      validAddress: undefined,
-      validAmount: undefined,
-      currency: 'part',
-      privacy: 50
-    };
-    this.send.subtractFeeFromAmount = false;
-  }
-
   clearReceiver(): void {
     this.send.toLabel = '';
     this.send.toAddress = '';
