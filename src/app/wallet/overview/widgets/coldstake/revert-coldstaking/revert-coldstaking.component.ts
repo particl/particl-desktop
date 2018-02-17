01f52a1e603685b6fea777bc2263cc4fe24b13f0
Claudio Cabral <cabral1349@gmail.com>
2018-02-17 13:22:09 +0100

    improves revert coldstaking

--- src/app/wallet/overview/widgets/coldstake/revert-coldstaking/revert-coldstaking.component.ts
+++ src/app/wallet/overview/widgets/coldstake/revert-coldstaking/revert-coldstaking.component.ts
@@ -17,7 +17,7 @@ export class RevertColdstakingComponent implements OnInit {
 
   private log: any = Log.create('revert-coldstaking');
 
-  public fee: number;
+  public fee: number = 0;
   public utxos: any = {
     txs: [],
     amount: 0
@@ -31,9 +31,7 @@ export class RevertColdstakingComponent implements OnInit {
     private _modals: ModalsService,
     private _rpc: RpcService,
     private _rpcState: RpcStateService,
-  ) { }
-
-  ngOnInit() {
+  ) {
     this.utxos = {
       txs: [],
       amount: 0
@@ -41,65 +39,65 @@ export class RevertColdstakingComponent implements OnInit {
 
     this._rpc.call('liststealthaddresses', null)
       .subscribe(stealthAddresses => {
-            try {
-                this.address = stealthAddresses[0]['Stealth Addresses'][0]['Address'];
-            } catch (err) {
-                this.address = '';
-                this.dialogRef.close();
-                this.flashNotification.open(
-                  'No stealth address found, please add a stealthaddress.', 'error');
-                return;
-            };
+	try {
+	  this.address = stealthAddresses[0]['Stealth Addresses'][0]['Address'];
+	} catch (err) {
+	  this.address = '';
+	  this.dialogRef.close();
+	  this.flashNotification.open(
+	    'No stealth address found, please add a stealthaddress.', 'error');
+	  return;
+	};
 
-            this.log.d('return address', this.address);
+	this.log.d('return address', this.address);
 
-            let sentTXs = 0;
-            let totalFee = 0;
+	let sentTXs = 0;
+	let totalFee = 0;
 
-            this._rpc.call('listunspent').subscribe(unspent => {
-              // TODO: Must process amounts as integers
-              unspent.map(utxo => {
-                if (!utxo.coldstaking_address
-                  || !utxo.address) {
-                  // skip
-                } else {
-                  this.utxos.amount += utxo.amount;
-                  this.utxos.txs.push({
-                    address: utxo.address,
-                    amount: utxo.amount,
-                    inputs: [{ tx: utxo.txid, n: utxo.vout }]
-                  });
-                };
-              });
+	this._rpc.call('listunspent').subscribe(unspent => {
+	  // TODO: Must process amounts as integers
+	  unspent.map(utxo => {
+	    if (!utxo.coldstaking_address
+	      || !utxo.address) {
+	      // skip
+	    } else {
+	      this.utxos.amount += utxo.amount;
+	      this.utxos.txs.push({
+		address: utxo.address,
+		amount: utxo.amount,
+		inputs: [{ tx: utxo.txid, n: utxo.vout }]
+	      });
+	    };
+	  });
 
-              this.utxos.txs.map(tx => {
+	  this.utxos.txs.map(tx => {
 
-                this.log.d('revert fee for address', tx);
+	    this.log.d('revert fee for address', tx);
 
-                this._rpc.call('sendtypeto', ['part', 'part', [{
-                  subfee: true,
-                  address: this.address,
-                  amount: tx.amount
-                }], '', '', 4, 64, true, JSON.stringify({
-                  inputs: tx.inputs
-                })]).subscribe(res => {
+	    this._rpc.call('sendtypeto', ['part', 'part', [{
+	      subfee: true,
+	      address: this.address,
+	      amount: tx.amount
+	    }], '', '', 4, 64, true, JSON.stringify({
+	      inputs: tx.inputs
+	    })]).subscribe(res => {
 
-                  sentTXs++;
-                  totalFee += res.fee;
-                  this.log.d(`revert ${sentTXs} fees`, res);
+	      sentTXs++;
+	      totalFee += res.fee;
+	      this.log.d(`revert ${sentTXs} fees`, res);
 
-                  if (sentTXs === this.utxos.txs.length) {
-                    this.fee = totalFee;
-                  }
-                }, error => {
-                  this.log.er('errr');
-                });
-              });
-            });
-        },
-        error => {
-          this.log.er('errr');
-        });
+	      if (sentTXs === this.utxos.txs.length) {
+		this.fee = totalFee;
+	      }
+	    }, error => {
+	      this.log.er('errr');
+	    });
+	  });
+	});
+      },
+	error => {
+	  this.log.er('errr');
+	});
   }
 
   revert() {
@@ -114,22 +112,22 @@ export class RevertColdstakingComponent implements OnInit {
       this.log.d('revert for address', tx);
 
       this._rpc.call('sendtypeto', ['part', 'part', [{
-        subfee: true,
-        address: this.address,
-        amount: tx.amount
+	subfee: true,
+	address: this.address,
+	amount: tx.amount
       }], 'revert coldstaking', '', 4, 64, false, JSON.stringify({
-        inputs: tx.inputs
+	inputs: tx.inputs
       })]).subscribe(res => {
 
-        this.log.d('revert response', res);
-        amount += tx.amount;
+	this.log.d('revert response', res);
+	amount += tx.amount;
 
-        if (++sentTXs === this.utxos.txs.length) {
-          this._rpcState.set('ui:coldstaking', false);
-          this.dialogRef.close();
-          this.flashNotification.open(
-            `Succesfully brought ${amount} PART into hot wallet`, 'warn');
-        }
+	if (++sentTXs === this.utxos.txs.length) {
+	  this._rpcState.set('ui:coldstaking', false);
+	  this.dialogRef.close();
+	  this.flashNotification.open(
+	    `Succesfully brought ${amount} PART into hot wallet`, 'warn');
+	}
       });
     });
 
@@ -143,9 +141,8 @@ export class RevertColdstakingComponent implements OnInit {
 
       this.log.d('disable coldstaking', res);
       if (res.changeaddress !== 'cleared') {
-        return false;
+	return false;
       }
-
       // TODO: update status of cold staking widget
     });
   }
