d2b83fd9036b0705086975262de6138081f4f293
Kewde <code@shadowproject.io>
2018-02-20 16:58:29 +0100

    add browseranimations

--- src/app/market/sell/add-item/add-item.component.spec.ts
+++ src/app/market/sell/add-item/add-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -15,6 +16,7 @@ describe('AddItemComponent', () => {
       declarations: [ AddItemComponent ],
       imports: [
         CoreUiModule.forRoot(),
+        BrowserAnimationsModule,
         RouterTestingModule
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
