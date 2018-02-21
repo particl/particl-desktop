26ef072858744cca91e8b84975eeef0c261446e9
Kewde <code@shadowproject.io>
2018-02-21 11:13:11 +0000

    rm unused import

--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, ModuleWithProviders } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreUiModule } from '../core-ui/core-ui.module';
