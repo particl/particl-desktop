b90a0f6c9a0a2d6df708fca1be884c48accbff14
Kewde <code@shadowproject.io>
2018-02-22 10:36:01 +0000

    update rpcState

--- src/app/wallet/wallet/send/send.component.ts
+++ src/app/wallet/wallet/send/send.component.ts
@@ -61,7 +61,7 @@ export class SendComponent implements OnInit {
 
   ngOnInit() {
     /* check if testnet -> Show/Hide Anon Balance */
-     this._rpc.state.observe('chain').take(1)
+     this.rpcState.observe('getblockchaininfo', 'chain').take(1)
      .subscribe(chain => this.testnet = chain === 'test');
   }
   /** Select tab */
