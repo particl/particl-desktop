ad8f87a1e59898a8806878232ea4106fb0274b52
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2018-02-22 17:46:55 +0100

    moved to electron-native-notification

--- modules/notification/notification.js
+++ modules/notification/notification.js
@@ -2,17 +2,15 @@ const rxIpc        = require('rx-ipc-electron/lib/main').default;
 const log          = require('electron-log');
 const path         = require('path');
 const Observable   = require('rxjs/Observable').Observable;
-const notification = require('electron-notify');
+const Notification = require('electron-native-notification');
 
 exports.init = function () {
-  console.log("notification init");
-  notification.setConfig({
-    appIcon: 'assets/icons/notification.png',
-    displayTime: 3000
-  });
   rxIpc.registerListener('notification', (title, desc, params) => {
-    console.log('got notification', title, desc, params);
-    notification.notify({ title: title, text: desc });
+    params = params ? params : { }; /* create param object if no param was given */
+    params.icon = '../../src/assets/icons/notification.png';
+    params.body = desc;
+    let notification = new Notification(title, params);
+    setTimeout(() => notification.close(), 3000);
     return Observable.create(observer => observer.complete(true));
   });
 }
