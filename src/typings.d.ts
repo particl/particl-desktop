5e10fd1aac22db5e9508e64bf3e32de4ddf94537
Kewde <code@shadowproject.io>
2018-02-22 01:31:02 +0000

    rm unused

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,7 +8,6 @@ interface NodeModule {
 
 interface Window {
   electron: boolean;
-  m: (method: string, params?: Array<any> | null) => any;
   require: any;
   ipc: {
     on: (channel: string, listener: Function) => void;
