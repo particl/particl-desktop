c1c9cf8a4ac1c97732712827203b39518b787c21
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2018-02-22 16:18:43 +0100

    cleaning PR

--- modules/notification/notification.js
+++ modules/notification/notification.js
@@ -1,26 +1,17 @@
-/* electron */
-const rxIpc = require('rx-ipc-electron/lib/main').default;
-const log = require('electron-log');
+const rxIpc        = require('rx-ipc-electron/lib/main').default;
+const log          = require('electron-log');
+const path         = require('path');
+const Observable   = require('rxjs/Observable').Observable;
+const notification = require('electron-notify');
 
-/* node */
-const path = require('path');
-
-const Observable = require('rxjs/Observable').Observable;
-
-/*
-    Register and IPC listener and execute notification.
-*/
 exports.init = function () {
-    var eNotify = require('electron-notify');
-    eNotify.setConfig({
-        appIcon: '../../src/assets/icons/notification.png',
+    notification.setConfig({
+        appIcon: 'assets/icons/notification.png',
         displayTime: 3000
     });
-    rxIpc.registerListener('notification', function (title, desc, params) {
-        eNotify.notify({ title: title, text: desc });
-        return Observable.create(observer => {
-            observer.complete(true);
-        });
+    rxIpc.registerListener('notification', (title, desc, params) => {
+        notification.notify({ title: title, text: desc });
+        return Observable.create(observer => observer.complete(true));
     });
 }
 
