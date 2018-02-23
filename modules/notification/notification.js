e4c8dcc3b89656cceb8949859923fd713fc27f97
vikas <vikas.v@cisinlabs.com>
2018-02-23 21:35:25 +0530

    path fix

--- modules/notification/notification.js
+++ modules/notification/notification.js
@@ -2,12 +2,13 @@ const rxIpc        = require('rx-ipc-electron/lib/main').default;
 const log          = require('electron-log');
 const path         = require('path');
 const Observable   = require('rxjs/Observable').Observable;
+const util = require('../util/util');
 const Notification = require('electron-native-notification');
 
 exports.init = function () {
   rxIpc.registerListener('notification', (title, desc, params) => {
     params = params ? params : { }; /* create param object if no param was given */
-    params.icon = '../../src/assets/icons/notification.png';
+    params.icon = util.getRootOrResourcePath();
     params.body = desc;
     let notification = new Notification(title, params);
     setTimeout(() => notification.close(), 3000);
---
+++ modules/util/util.js
@@ -0,0 +1,20 @@
+const { app } = require('electron');
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+const log = require('electron-log');
+
+
+function getRootOrResourcePath() {
+  var dir; 
+  // running from packaged
+  if(__dirname.search('app.asar') > -1) {
+    dir = __dirname.substring(0, __dirname.indexOf('app.asar')) + 'app.asar';
+    dir = path.join(dir, 'src/assets/icons/notification.png');
+  } else {
+    dir = '../../src/assets/icons/notification.png';
+  }
+  return dir;
+}
+
+exports.getRootOrResourcePath = getRootOrResourcePath;
