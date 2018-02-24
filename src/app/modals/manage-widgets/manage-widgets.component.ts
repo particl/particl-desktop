c50658fabbfa5fc79b47e3e0f3a89c87a27ad021
Martin Allien <martin@allienworks.net>
2018-02-24 23:12:31 +0100

    Manage widgets modal styled

--- src/app/modals/manage-widgets/manage-widgets.component.ts
+++ src/app/modals/manage-widgets/manage-widgets.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
-// import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-widgets',
