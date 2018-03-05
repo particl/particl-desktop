0bb89c29dbd5c35469f862d652788bf94d2b9c10
Kirti Anand <kirti.a@cisinlabs.com>
2018-03-05 22:55:57 +0530

    getFavorites item  in OverviewListingsComponent

--- src/app/market/overview-listings/overview-listings.component.ts
+++ src/app/market/overview-listings/overview-listings.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Log } from 'ng2-logger';
 
-import { MarketService } from 'app/core/market/market.service';
 import { MarketStateService } from 'app/core/market/market-state/market-state.service';
 
 import { Category } from 'app/core/market/api/category/category.model';
@@ -60,7 +59,7 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
     maxPerPage: 30,
     // hooks into the scroll bar of the main page..
     infinityScrollSelector: '.mat-drawer-content' // .mat-drawer-content
-  }
+  };
 
   filters: any = {
     search:   undefined
@@ -68,7 +67,8 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
 
   constructor(
     private category: CategoryService,
-    private listingService: ListingService
+    private listingService: ListingService,
+    private marketState: MarketStateService
   ) {
     console.warn('overview created');
   }
@@ -77,6 +77,18 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
     console.log('overview created');
     this.loadCategories();
     this.loadPage(1);
+
+    this.getFavorites();
+  }
+
+  getFavorites() {
+    this.marketState.observe('favorite').subscribe(res => {
+
+      if (this.pages[0] && this.pages[0].listings) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>############>>>>>>>>>>>>', this.pages[0].listings);
+        // @TODO compare two array and find fav. item
+      }
+    })
   }
 
   loadCategories() {
@@ -94,26 +106,28 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
 
     const search = this.filters.search;
 
-    this.listingService.search(pageNumber, max, null, search).take(1).subscribe((listings: Array<any>) => {
-      // new page
-      const page = {
-        pageNumber: pageNumber,
-        listings: listings.map(listing => new Template(listing))
-      };
+    this.listingService.search(pageNumber, max, null, search)
+      .take(1)
+      .subscribe((listings: Array<any>) => {
+        // new page
+        const page = {
+          pageNumber: pageNumber,
+          listings: listings.map(listing => new Template(listing))
+        };
 
-      // should we clear all existing pages? e.g search
-      if (clear === true) {
-        this.pages = [page];
-        this.noMoreListings = false;
-      } else { // infinite scroll
-        if (listings.length > 0) {
-          this.pushNewPage(page);
-        } else {
-          this.noMoreListings = true;
+        // should we clear all existing pages? e.g search
+        if (clear === true) {
+          this.pages = [page];
+          this.noMoreListings = false;
+        } else { // infinite scroll
+          if (listings.length > 0) {
+            this.pushNewPage(page);
+          } else {
+            this.noMoreListings = true;
+          }
         }
-      }
 
-    })
+      })
   }
 
   pushNewPage(page: IPage) {
@@ -146,10 +160,11 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
 
   // TODO: fix scroll up!
   loadPreviousPage() {
-    console.log("prev page trigered");
-    let previousPage = this.getFirstPageCurrentlyLoaded(); previousPage--;
-    console.log("loading prev page" + previousPage);
-    if(previousPage > 0) {
+    console.log('prev page trigered');
+    let previousPage = this.getFirstPageCurrentlyLoaded();
+    previousPage--;
+    console.log('loading prev page' + previousPage);
+    if (previousPage > 0) {
       this.loadPage(previousPage);
     }
   }
@@ -165,7 +180,7 @@ export class OverviewListingsComponent implements OnInit, OnDestroy {
     return this.pages[this.pages.length - 1].pageNumber;
   }
 
-  // Returns the pageNumber if the first page that is currently visible 
+  // Returns the pageNumber if the first page that is currently visible
   getFirstPageCurrentlyLoaded() {
     return this.pages[0].pageNumber;
   }
