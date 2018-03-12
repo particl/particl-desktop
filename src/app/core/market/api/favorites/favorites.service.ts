44fb03bad0c16f365fcd6d4140e8594fc6f440a5
Kirti Anand <kirti.a@cisinlabs.com>
2018-03-12 19:07:28 +0530

    Adds storeFavorites & heck favorite methods

--- src/app/core/market/api/favorites/favorites.service.ts
+++ src/app/core/market/api/favorites/favorites.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 
 import { MarketService } from 'app/core/market/market.service';
+import { MarketStateService } from '../../market-state/market-state.service';
+import { Favorite } from './favorite.model';
 
 @Injectable()
 export class FavoritesService {
 
+  public favorites: Favorite[];
   constructor(
-    private market: MarketService
-  ) { }
+    private market: MarketService,
+    private marketState: MarketStateService
+  ) {
+    this.storeFavorites();
+  }
 
   addItem(id: number) {
     return this.market.call('favorite', ['add', 1, id]);
@@ -19,5 +26,18 @@ export class FavoritesService {
 
   getFavorites() {
     return this.market.call('favorite', ['list', 1])
+  }
+
+  storeFavorites() {
+    this.marketState.observe('favorite').subscribe((favorite: Favorite[]) => {
+      this.favorites = favorite;
+    });
+  }
+
+  isListingItemFavorited(listingItemId: number): boolean {
+    const index = _.findIndex(this.favorites, function (obj: Favorite) {
+      return obj.listingItemId === listingItemId;
+    });
+    return (index > -1)
   }
 }
