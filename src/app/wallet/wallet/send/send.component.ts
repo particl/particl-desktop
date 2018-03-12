c651baf6667a652333b3b5c7603d46c1582732fd
Kirti Anand <kirti.a@cisinlabs.com>
2018-03-12 21:47:57 +0530

    Remove checkWalletIslocked from sendComponent

--- src/app/wallet/wallet/send/send.component.ts
+++ src/app/wallet/wallet/send/send.component.ts
@@ -168,29 +168,12 @@ export class SendComponent implements OnInit {
   }
 
   onSubmit(): void {
-    // @TODO refactor unlock wallet checking
-    // this.checkWalletIslocked(this.openSendConfirmationModal, 30);
     if (this._rpcState.get('locked')) {
       // unlock wallet and send transaction
       this._modals.open('unlock', {forceOpen: true, timeout: 30, callback: this.openSendConfirmationModal.bind(this)});
     } else {
       // wallet already unlocked
       this.openSendConfirmationModal();
-    }
-  }
-
-  /**
-    * Check wallet is locked
-    * @param callback
-    */
-
-  checkWalletIslocked(callback: any, timeout: number = 10) {
-    if (this._rpcState.get('locked')) {
-      // unlock wallet and send transaction
-      this._modals.open('unlock', {forceOpen: true, timeout: 10, callback: callback.bind(this)});
-    } else {
-      // wallet already unlocked
-      callback();
     }
   }
 
@@ -248,7 +231,13 @@ export class SendComponent implements OnInit {
 
     }
 
-    this.checkWalletIslocked(this.sendTransaction);
+    if (this._rpcState.get('locked')) {
+      // unlock wallet and send transaction
+      this._modals.open('unlock', {forceOpen: true, timeout: 30, callback: this.sendTransaction.bind(this)});
+    } else {
+      // wallet already unlocked
+      this.sendTransaction();
+    }
   }
 
   private sendTransaction(): void {
