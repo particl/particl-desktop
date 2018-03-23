f9f681f810e3cfc9b4532fdf5bbecb0c83322639
Kirti Anand <kirti.a@cisinlabs.com>
2018-03-23 15:25:32 +0530

    Creates shared/favoriteComponent

---
+++ src/app/market/shared/favorite/favorite.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FavoriteComponent } from './favorite.component';
+
+describe('FavoriteComponent', () => {
+  let component: FavoriteComponent;
+  let fixture: ComponentFixture<FavoriteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FavoriteComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FavoriteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/market/shared/favorite/favorite.component.ts
@@ -0,0 +1,46 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+import { FavoritesService } from '../../../core/market/api/favorites/favorites.service';
+import { SnackbarService } from '../../../core/snackbar/snackbar.service';
+import { MarketStateService } from '../../../core/market/market-state/market-state.service';
+
+import { Listing } from '../../../core/market/api/listing/listing.model';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.scss']
+})
+export class FavoriteComponent implements OnInit {
+
+  @Input() listing: Listing;
+
+  constructor(
+    private favoritesService: FavoritesService,
+    private snackbar: SnackbarService,
+    private marketState: MarketStateService
+  ) {}
+
+  ngOnInit() {
+  }
+
+  addToFavorites() {
+    if (this.listing.favorite) {
+      this.favoritesService.removeItem(this.listing.id).take(1).subscribe(res => {
+        this.updateFavorites();
+        this.snackbar.open(`${this.listing.title} removed from Favorite`);
+        this.listing.favorite = false;
+      });
+    } else {
+      this.favoritesService.addItem(this.listing.id).take(1).subscribe(res => {
+        this.updateFavorites();
+        this.snackbar.open(`${this.listing.title} added to Favorite`);
+        this.listing.favorite = true;
+      });
+    }
+  }
+
+  updateFavorites() {
+    this.marketState.registerStateCall('favorite', null, ['list', 1]);
+  }
+}
