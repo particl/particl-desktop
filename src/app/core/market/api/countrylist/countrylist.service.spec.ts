c03a4a0b8466eef87a3c65aa5300bcdf202c0416
vikas <vikas.v@cisinlabs.com>
2018-03-27 16:30:07 +0530

    test case for countrylist service

--- src/app/core/market/api/countrylist/countrylist.service.spec.ts
+++ src/app/core/market/api/countrylist/countrylist.service.spec.ts
@@ -11,4 +11,19 @@ describe('CountryListService', () => {
   it('should be created', inject([CountryListService], (service: CountryListService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get country list', inject([CountryListService], (service: CountryListService) => {
+    expect(service.getList().length).toBeGreaterThan(0);
+  }));
+
+  it('should search country by name', inject([CountryListService], (service: CountryListService) => {
+    let mockString = 'Andorra'
+    expect(service.getCountryByName(mockString).name).toBe(mockString);
+  }));
+
+  it('should return nil country ', inject([CountryListService], (service: CountryListService) => {
+    let mockString = 'and'
+    expect(service.getCountryByName(mockString)).toBe(undefined);
+  }));
+
 });
