97f71d80b2e7332a00cf34bc31742d7f8a6a0ca6
rynomster <rmathee@gmail.com>
2018-04-01 00:04:37 +0200

    Refactor - reuse existing Amount

--- src/app/core/market/api/template/template.model.ts
+++ src/app/core/market/api/template/template.model.ts
@@ -74,40 +74,27 @@ export class Template {
     return this.object.ItemInformation.ItemImages;
   }
 
-  get intPrice(): any {
-    const itemPrice = this.getPrice();
-    if (itemPrice) {
-      return new Amount(itemPrice.basePrice).getIntegerPart();
-    }
-    return undefined;
+  get intPrice(): number {
+    return this.basePrice.getIntegerPart();
   }
 
-  get centsPrice(): any {
-    const itemPrice = this.getPrice();
-    if (itemPrice) {
-      return new Amount(itemPrice.basePrice).getFractionalPart();
-    }
-    return undefined;
+  get centsPrice(): number {
+    return this.basePrice.getFractionalPart();
   }
 
   setBasePrice(): void {
-    const itemPrice = this.getPrice();
-    if (itemPrice) {
-      this.basePrice = new Amount(itemPrice.basePrice);
-    } else {
-      this.basePrice = undefined;
-    }
+    this.basePrice = (this.object.PaymentInformation.ItemPrice
+      ? new Amount(this.object.PaymentInformation.ItemPrice.basePrice)
+      : undefined);
   }
 
   setShippingPrice(): void {
-    const itemPrice = this.getPrice();
+    const itemPrice = this.object.PaymentInformation.ItemPrice;
     if (itemPrice && itemPrice.ShippingPrice) {
-      this.domesticShippingPrice = new Amount(itemPrice.ShippingPrice.domestic);
-      this.internationalShippingPrice = new Amount(itemPrice.ShippingPrice.international);
+      this.domesticShippingPrice = (itemPrice.ShippingPrice.domestic
+        ? new Amount(itemPrice.ShippingPrice.domestic) : undefined);
+      this.internationalShippingPrice = (itemPrice.ShippingPrice.international
+        ? new Amount(itemPrice.ShippingPrice.international) : undefined);
     }
-  }
-
-  getPrice(): any {
-    return this.object.PaymentInformation.ItemPrice;
   }
 }
