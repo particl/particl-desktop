f06c9f21642fbef854a8e0300d04591eed03e047
Kirti Anand <kirti.a@cisinlabs.com>
2018-03-31 22:19:09 +0530

    Creates shippingDetails model

---
+++ src/app/market/shared/shipping-details.model.ts
@@ -0,0 +1,10 @@
+export class ShippingDetails {
+  firstName: string;
+  lastName: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  zip: string;
+  state: string;
+  country: string;
+}
