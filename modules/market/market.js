d04c6091c55379ed9461515cda18a597af150e0e
Kewde <kewde@particl.io>
2018-04-24 15:12:01 +0200

    implement migration, .env creation, datadir

--- modules/market/market.js
+++ modules/market/market.js
@@ -4,7 +4,22 @@ exports.init = function() {
   const options = _options.get();
 
   if (!options.skipmarket) {
-    const mp = require('../../particl-market/dist/app');
+    const datadir = require('../../particl-market/dist/core/helpers/DataDir');
+    console.log("particl-market: ", datadir.DataDir.getDataDirPath());
+    datadir.DataDir.initialize().then((result) => {
+      console.log("particl-market initialized: ", result);
+      datadir.DataDir.createDefaultEnvFile().then((env) => {
+        console.log("particl-market env created?: ", env);
+
+        const migrate = require('../../particl-market/dist/database/migrate');
+        migrate.migrate().then(
+          console.log('Migration done')
+        );
+        
+        const mp = require ('../../particl-market/dist/app');
+      });
+    })
+
   }
 }
 
