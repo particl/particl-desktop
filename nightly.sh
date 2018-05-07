be87fedb7e5cfc580ff6a44aefe3efb61b2d5257
Kewde <kewde@particl.io>
2018-05-08 00:33:37 +0200

    expire builds after 5 days +build -mac

--- nightly.sh
+++ nightly.sh
@@ -56,7 +56,7 @@ then
     Uploads=("Builds\n")
     for fn in `ls | grep "particl-desktop"`; do
         echo "Uploading $fn"
-        url="$(curl -s --upload-file $fn https://transfer.sh/$fn)\n\n"
+        url="$(curl -s -H "Max-Days: 5"  --upload-file $fn https://transfer.sh/$fn)\n\n"
         checksum="$(sha256sum $fn)\n"
         Uploads=(${Uploads[@]} $checksum)
         Uploads=(${Uploads[@]} $url)
