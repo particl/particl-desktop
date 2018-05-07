03ddfca951b9f5c7059da8756578a171895cee35
Kewde <kewde@particl.io>
2018-05-07 13:41:31 +0200

    Try few commands +build -linux -mac -upload

--- nightly.sh
+++ nightly.sh
@@ -26,6 +26,8 @@ then
     sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ trusty main'
     sudo apt-get update
     sudo apt-get install -y --install-recommends winehq-devel
+    wine regedit /d 'HKEY_LOCAL_MACHINE\\Software\\Microsoft\Windows\CurrentVersion\Explorer\Desktop\Namespace\{9D20AAE8-0625-44B0-9CA7-71889C2254D9}'
+    echo disable > "${WINEPREFIX:-${HOME}/.wine}/.update-timestamp"
     DEBUG=electron-builder yarn run package:win
     ls -l /home/travis/.wine
     echo -en 'travis_fold:end:script.win\\r'
