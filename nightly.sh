57d6e75fe83d21b00c3f3bfeebbb300ced3b59a7
Kewde <kewde@particl.io>
2018-05-07 22:13:28 +0200

    seperate win builds

--- nightly.sh
+++ nightly.sh
@@ -20,16 +20,19 @@ fi
 if [[ $TRUE_COMMIT_MESSAGES != *"-win"* ]]
 then
     echo 'Win build' && echo -en 'travis_fold:start:script.win\\r'
-    sudo dpkg --add-architecture i386
-    wget -nc https://dl.winehq.org/wine-builds/Release.key
-    sudo apt-key add Release.key
-    sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-    sudo apt-get update -y
-    sudo apt-get install -y --force-yes wine-stable winehq-stable
-    WINEARCH=win32 winecfg
+#    sudo dpkg --add-architecture i386
+#    wget -nc https://dl.winehq.org/wine-builds/Release.key
+#    sudo apt-key add Release.key
+#    sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+#    sudo apt-get update -y
+#    sudo apt-get install -y --force-yes wine-stable winehq-stable
+#    WINEARCH=win32 winecfg
+
 #    wine regedit /d 'HKEY_LOCAL_MACHINE\\Software\\Microsoft\Windows\CurrentVersion\Explorer\Desktop\Namespace\{9D20AAE8-0625-44B0-9CA7-71889C2254D9}'
 #    echo disable > "${WINEPREFIX:-${HOME}/.wine}/.update-timestamp"
-    DEBUG=electron-builder yarn run package:win
+
+    DEBUG=electron-builder yarn run package:win32
+    DEBUG=electron-builder yarn run package:win64
     ls -l ./packages
     echo -en 'travis_fold:end:script.win\\r'
 fi
