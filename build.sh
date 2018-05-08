d3fb69d9191ef9d58ca0e7af6ebbe02844621863
Kewde <kewde@particl.io>
2018-05-08 00:53:35 +0000

    test all builds +builds
    +builds

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker run --rm \
-    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_|TRUE_COMMIT_MESSAGEs|GITHUB_TOKEN') \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_|TRUE_COMMIT_MESSAGES|GITHUB_TOKEN') \
     -v ${PWD}:/project \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
