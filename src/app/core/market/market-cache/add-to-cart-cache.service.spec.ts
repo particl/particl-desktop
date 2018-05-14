117ade4bc695944c9e02d006f2f82c6d68ed2a17
vikas <vikas.v@cisinlabs.com>
2018-05-14 15:22:20 +0530

    test fix for cache service

--- src/app/core/market/market-cache/add-to-cart-cache.service.spec.ts
+++ src/app/core/market/market-cache/add-to-cart-cache.service.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { MarketModule } from 'app/core/market/market.module';
+import { MarketCacheModule } from 'app/core/market/market-cache/market-cache.module';
+
 import { AddToCartCacheService } from './add-to-cart-cache.service';
 
 describe('AddToCartCacheService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MarketModule.forRoot(),
+        MarketCacheModule.forRoot()
+      ],
       providers: [AddToCartCacheService]
     });
   });
