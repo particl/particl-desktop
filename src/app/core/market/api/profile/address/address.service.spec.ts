32ee1188d91acc50e184370abf0f509add63a849
Kewde <kewde@particl.io>
2018-05-19 01:42:50 +0200

    fix test

--- src/app/core/market/api/profile/address/address.service.spec.ts
+++ src/app/core/market/api/profile/address/address.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { MarketModule } from 'app/core/market/market.module';
+
 import { AddressService } from './address.service';
 
 describe('AddressService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MarketModule.forRoot()
+      ],
       providers: [AddressService]
     });
   });
