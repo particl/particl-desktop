8e36d696a5d77202712e9f5e6ab5746a6d732d87
vikas <vikas.v@cisinlabs.com>
2018-05-22 14:49:20 +0530

    small change

--- src/app/core/market/api/template/image/imagecollection.model.ts
+++ src/app/core/market/api/template/image/imagecollection.model.ts
@@ -1,16 +1,14 @@
 import { Image, DefaultImage } from './image.model';
 
 export class ImageCollection {
+  private default: Image = new DefaultImage();
   constructor(private images: Image[]) {
     this.setImages();
   }
 
   get featuredImage(): Image {
     const itemimage = this.images[0];
-    if (itemimage) {
-      return itemimage;
-    }
-    return new DefaultImage();
+    return itemimage || this.default;
   }
 
   setImages() {
