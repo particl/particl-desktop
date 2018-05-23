8d64f3983adba10e7258b295f43431334a565b62
Kewde <kewde@particl.io>
2018-05-24 01:33:01 +0200

    fix test

--- src/app/core/updater/updater.service.spec.ts
+++ src/app/core/updater/updater.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material';
 
 import { IpcService } from '../ipc/ipc.service';
 import { UpdaterService } from './updater.service';
 
+
 describe('UpdaterService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ MatDialogModule ],
       providers: [UpdaterService, IpcService]
     });
   });
