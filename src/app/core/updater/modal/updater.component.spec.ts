00add76c72b92b6175d08eb30a66f289563917c0
Kewde <kewde@particl.io>
2018-05-24 16:00:04 +0000

    add MatDialog to test

--- src/app/core/updater/modal/updater.component.spec.ts
+++ src/app/core/updater/modal/updater.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material';
 
 import { UpdaterComponent } from './updater.component';
 
@@ -8,6 +9,7 @@ describe('UpdaterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MatDialogModule ],
       declarations: [ UpdaterComponent ]
     })
     .compileComponents();
