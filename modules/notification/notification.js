0fbbd69649d8cf4b92411a2491fb777a747ecb8d
Kewde <kewde@particl.io>
2018-06-07 20:47:45 +0200

    revert to old notification

--- modules/notification/notification.js
+++ modules/notification/notification.js
@@ -1,22 +1,31 @@
-const rxIpc        = require('rx-ipc-electron/lib/main').default;
-const log          = require('electron-log');
-const path         = require('path');
-const Observable   = require('rxjs/Observable').Observable;
-const util = require('../util/util');
-const Notification = require('electron-native-notification');
+/* electron */
+const rxIpc = require('rx-ipc-electron/lib/main').default;
+const log = require('electron-log');
+const Notification = require('electron').Notification;
 
+/* node */
+const path = require('path');
+
+const Observable = require('rxjs/Observable').Observable;
+
+/*
+    Register and IPC listener and execute notification.
+*/
 exports.init = function () {
-  rxIpc.registerListener('notification', (title, desc, params) => {
-    params = params ? params : { }; /* create param object if no param was given */
-    params.icon = util.getRootOrResourcePath();
-    params.body = desc;
-    let notification = new Notification(title, params);
-    setTimeout(() => notification.close(), 3000);
-    return Observable.create(observer => observer.complete(true));
-  });
+    rxIpc.registerListener('notification', function (title, desc, params) {
+        let notification = new Notification({
+            'title': title,
+            'body': desc,
+            'icon': util.getRootOrResourcePath()
+        })
+        notification.show()
+        return Observable.create(observer => {
+            observer.complete(true);
+        });
+    });
 }
 
 // todo: test
 exports.destroy = function() {
-  rxIpc.removeListeners('notification');
+    rxIpc.removeListeners('notification');
 }
