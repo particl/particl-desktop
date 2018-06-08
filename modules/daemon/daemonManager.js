279df2e8aa5c7ff95318d3af7dca47260d69d332
Kewde <kewde@particl.io>
2018-06-08 02:17:11 +0200

    update sqlite3 update particl-market 0.0.27 launch in seperate process

--- modules/daemon/daemonManager.js
+++ modules/daemon/daemonManager.js
@@ -19,7 +19,7 @@ let options;
 // dev
 // const BINARY_URL = 'https://raw.githubusercontent.com/particl/particl-desktop/develop/modules/clientBinaries/clientBinaries.json';
 
-const BINARY_URL = 'https://raw.githubusercontent.com/particl/particl-desktop/market/modules/clientBinaries/clientBinaries.json';
+const BINARY_URL = 'https://raw.githubusercontent.com/particl/particl-desktop/core-0.16.0.7/modules/clientBinaries/clientBinaries.json';
 
 //const ALLOWED_DOWNLOAD_URLS_REGEX = new RegExp('*', 'i');
 
