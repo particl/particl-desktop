f2e2db4172eabeab74ed8bc4627239881f5a35bc
vikas <vikas.v@cisinlabs.com>
2018-06-11 18:03:45 +0530

    resolved error handle

--- src/app/core/rpc/rpc.service.ts
+++ src/app/core/rpc/rpc.service.ts
@@ -108,7 +108,7 @@ export class RpcService implements OnDestroy {
             } else if (error._body) {
               err = JSON.parse(error._body);
             } else {
-              err = error.message;
+              err = error.error ? error.error.error : error.message;
             }
 
             return Observable.throw(err)
