c0850326eb821ceedc7a4c689fe1020738f0d64c
vikas <vikas.v@cisinlabs.com>
2018-07-07 14:14:38 +0530

    test fix and lint fix

--- src/app/core/terms/terms.service.spec.ts
+++ src/app/core/terms/terms.service.spec.ts
@@ -2,15 +2,17 @@ import { TestBed, inject } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreModule } from './../core.module';
 import { TermsService } from './terms.service';
 
 describe('TermsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-				BrowserAnimationsModule,
-				MatDialogModule
-			]
+        BrowserAnimationsModule,
+        MatDialogModule,
+        CoreModule.forRoot()
+      ],
       providers: [ TermsService ]
     });
   });
