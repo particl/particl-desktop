17e09eeb35163bbe5501ed875b6a12aa17b891e6
vikas <vikas.v@cisinlabs.com>
2018-07-11 17:42:51 +0530

    test fix

--- src/app/modals/terms/terms.component.spec.ts
+++ src/app/modals/terms/terms.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef } from '@angular/material';
 import { TermsComponent } from './terms.component';
+import { CoreUiModule } from '../../core-ui/core-ui.module';
 
 describe('TermsComponent', () => {
   let component: TermsComponent;
@@ -8,7 +9,7 @@ describe('TermsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ MatDialogModule ],
+      imports: [ MatDialogModule, CoreUiModule.forRoot() ],
       declarations: [ TermsComponent ],
       providers: [
         /* deps */
