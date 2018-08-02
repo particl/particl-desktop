370503b2abd26005c5946e3b69860c6b203bb20e
Shubham T <shubham.t@cisinlabs.com>
2018-08-02 13:21:04 +0530

    update proposal and vote option models

--- src/app/wallet/proposals/models/proposal.ts
+++ src/app/wallet/proposals/models/proposal.ts
@@ -11,28 +11,28 @@ export class Proposal {
   public type: string;
   public hash: string;
 
-  constructor(obj: any) {
-    this.id = obj.id;
-    this.title = obj.title;
-    this.options = obj.options.map(v => new VoteOption(v));
-    this.description = obj.description;
-    this.hash = obj.hash;
-    this.type = obj.type;
-    this.submitter = obj.submitter;
-    this.blockStart = obj.blockStart;
-    this.blockEnd = obj.blockEnd;
+  constructor(object: any) {
+    this.id = object.id;
+    this.title = object.title;
+    this.options = object.ProposalOptions.map(v => new VoteOption(v));
+    this.description = object.description;
+    this.hash = object.hash;
+    this.type = object.type;
+    this.submitter = object.submitter;
+    this.blockStart = object.blockStart;
+    this.blockEnd = object.blockEnd;
   }
 
   public isActiveProposal(currentBlockCount: number): boolean {
     return currentBlockCount < this.blockEnd;
   }
 
-  public leftVotingEndBlockCount(currentBlockCount: number) {
+  public leftVotingEndBlockCount(currentBlockCount: number): number {
     const leftBlocks = this.blockEnd - currentBlockCount;
     return leftBlocks > 0 ? leftBlocks : 0;
   }
 
-  public votingProgressPercentage(currentBlockCount: number) {
+  public votingProgressPercentage(currentBlockCount: number): number {
     const blocks = this.blockEnd - this.blockStart;
     const leftBlock = this.leftVotingEndBlockCount(currentBlockCount);
     if (!leftBlock) {
--- src/app/wallet/proposals/models/vote-option.ts
+++ src/app/wallet/proposals/models/vote-option.ts
@@ -1,9 +1,19 @@
 export class VoteOption {
   public optionId: number;
   public description: string;
+  createdAt: Date;
+  hash: string;
+  id: number;
+  proposalId: string;
+  updatedAt: Date;
 
-  constructor(obj: any) {
-    this.optionId = obj.optionId;
-    this.description = obj.description;
+  constructor(object: any) {
+    this.optionId = object.optionId;
+    this.description = object.description;
+    this.createdAt = object.createdAt;
+    this.hash = object.hash;
+    this.id = object.id;
+    this.proposalId = object.proposalId;
+    this.updatedAt = object.updatedAt;
   }
 }
