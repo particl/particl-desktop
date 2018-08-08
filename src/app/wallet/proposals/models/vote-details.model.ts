58973b94f6d9d6604de4b5d14a8be0bba191a9f4
Shubham T <shubham.t@cisinlabs.com>
2018-08-08 18:23:48 +0530

    - Added vote details model

---
+++ src/app/wallet/proposals/models/vote-details.model.ts
@@ -0,0 +1,22 @@
+import { VoteOption } from 'app/wallet/proposals/models/vote-option.model';
+
+export class VoteDetails {
+    ProposalOption: VoteOption
+    createdAt: Date;
+    id: number;
+    proposalOptionId: number;
+    updatedAt: Date;
+    voter: string;
+    weight: number;
+
+    constructor(object: any) {
+      this.ProposalOption = new VoteOption(object.ProposalOption);
+      this.createdAt = object.createdAt;
+      this.id = object.id;
+      this.proposalOptionId = object.ProposalOption.id;
+      this.updatedAt = object.updatedAt;
+      this.voter = object.voter;
+      this.weight = object.weight;
+    }
+  }
+
