f19f054ae89f50e61fe9b25f8c419cc9994a7ade
vikas <vikas.v@cisinlabs.com>
2018-08-30 14:27:33 +0530

    more fix for minmize electron

--- main.js
+++ main.js
@@ -132,7 +132,11 @@ function initMainWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    if (app.appQuitting) {
+      mainWindow = null
+    } else {
+      mainWindow.minimize(); 
+    }
   });
 }
 
