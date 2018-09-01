a540ad735b3744cbaebeb33e919e5889517e27ae
Shubham T <shubham.t@cisinlabs.com>
2018-09-01 19:14:19 +0530

    - refactored settings service. - Move the default settings in utils and used the same. - Create a rpc state for currentGUISettings.

--- src/app/wallet/settings/settings.service.ts
+++ src/app/wallet/settings/settings.service.ts
@@ -4,92 +4,28 @@ import { MarketService } from 'app/core/market/market.service';
 import { ProfileService } from 'app/core/market/api/profile/profile.service';
 import { Profile } from 'app/core/market/api/profile/profile.model';
 import { SettingsGuiService } from 'app/core/settings-gui/settings-gui.service';
+import { Settings } from 'app/wallet/settings/models/settings.model';
+import { RpcStateService } from 'app/core/rpc/rpc-state/rpc-state.service';
+import { DEFAULT_GUI_SETTINGS } from 'app/core/util/utils';
 
 @Injectable()
 export class SettingsService {
 
-  needsUpdate: boolean = false;
-  currentSettings: any;
-  defaultSettings: Object = {
-    main: {
-      autostart: false,
-      detachDatabases: 1,
-      feeAmount: 1,
-      feeCurrency: 1,
-      autoRing: 1,
-      minRing: 1,
-      maxRing: 1,
-      stake: 1,
-      reserveAmount: 1,
-      reserveCurrency: 1,
-      rewardAddressEnabled: 1,
-      rewardAddress: 1,
-      foundationDonation: 1,
-      secureMessaging: false,
-      thin: false,
-      thinFullIndex: false,
-      thinIndexWindow: 4096,
-      stakeInterval: 30
-    },
-    network: {
-      upnp: false,
-      proxy: false,
-      proxyIP: '127.0.0.1',
-      proxyPort: 9050,
-      socketVersion: 5
-    },
-    window: {
-      tray: false,
-      minimize: true
-    },
-    display: {
-      language: 'en',
-      units: 'part',
-      theme: 'light',
-      rows: 10,
-      addresses: true,
-      notify: {
-        message: true,
-        sentTo: false,
-        receivedWith: false,
-        receivedFrom: false,
-        selfPayment: false,
-        partReceived: true,
-        partSent: false,
-        other: false
-      },
-      show: {
-        sentTo: true,
-        receivedWith: true,
-        receivedFrom: true,
-        selfPayment: true,
-        partReceived: true,
-        partSent: true,
-        other: true
-      },
-    },
-    navigation: {
-      marketExpanded: true,
-      walletExpanded: true
-    },
-    market: {
-      enabled: true,
-      listingsPerPage: 30,
-      defaultCountry: undefined,
-      listingExpiration: 4
-    },
-    i2p: {},
-    tor: {}
-  };
+  defaultSettings: Settings;
 
   profileId: number;
+  currentSettings: Settings;
 
   constructor(
     private _rpc: RpcService,
+    private _rpcState: RpcStateService,
     private marketService: MarketService,
     private profileService: ProfileService,
     private _settingsGUIService: SettingsGuiService,
   ) {
+
+    this.defaultSettings = new Settings(DEFAULT_GUI_SETTINGS);
+
     this.profileService.default().subscribe((profile: Profile) => {
       this.profileId = profile.id;
     })
@@ -98,17 +34,16 @@ export class SettingsService {
 
     /* Preload default settings if none found */
     if (localStorage.getItem('gui-settings') == null) {
-      this.currentSettings = this.defaultSettings;
+      this.currentSettings = new Settings(this.defaultSettings);
       const settings: string = JSON.stringify(this.defaultSettings);
       localStorage.setItem('gui-settings', settings);
 
     } else {
-
-      this.currentSettings = this.loadSettings();
-
-      // this.list().subscribe((settings: any) => {
-      // })
+      this.currentSettings = new Settings(this.loadSettings());
     }
+
+    // set currentGUISettings state.
+    this._rpcState.set('currentGUISettings', this.currentSettings);
     this._settingsGUIService.updateSettings(this.currentSettings);
   }
 
@@ -120,10 +55,19 @@ export class SettingsService {
     const oldSettings: string = localStorage.getItem('gui-settings');
     const newSettings: string = JSON.stringify(settings);
 
-    localStorage.setItem('gui-settings', newSettings);
-    this._settingsGUIService.updateSettings(settings);
     if (oldSettings !== newSettings) {
-      this.needsUpdate = true;
+
+      // update web-storage gui-settings.
+      localStorage.setItem('gui-settings', newSettings);
+
+      // update core settings.
+      this._settingsGUIService.updateSettings(settings);
+
+      // update current settings
+      this.currentSettings = new Settings(settings);
+
+      // set currentGUISettings state.
+      this._rpcState.set('currentGUISettings', this.currentSettings);
     }
   }
 
