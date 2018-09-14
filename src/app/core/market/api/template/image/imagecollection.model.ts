8242770c9f4c222a52bfb46be1fab838ef4ec741
vikas <vikas.v@cisinlabs.com>
2018-09-14 16:32:16 +0530

    preview images on the basis of selection

--- src/app/core/market/api/template/image/imagecollection.model.ts
+++ src/app/core/market/api/template/image/imagecollection.model.ts
@@ -2,17 +2,26 @@ import { Image, DefaultImage } from './image.model';
 
 export class ImageCollection {
   private default: Image = new DefaultImage();
+  private image: Image;
   constructor(public images: Image[]) {
     this.setImages();
+    this.image = this.images[0]
   }
 
   get featuredImage(): Image {
-    const itemimage = this.images[0];
-    return itemimage || this.default;
+    return this.images[0] || this.default;
+  }
+
+  get previewImage(): Image {
+    return this.image || this.default;
   }
 
   setImages() {
     this.images = this.images.map(img => new Image(img))
+  }
+
+  setPreviewImage(image: Image) {
+    this.image = image;
   }
 
 }
