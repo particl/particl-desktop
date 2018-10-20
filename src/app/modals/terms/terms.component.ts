b453b0a1e192eedd454332429517e6cc82e2a87c
vikas <vikas.v@cisinlabs.com>
2018-10-20 20:37:45 +0530

    scroll fix

--- src/app/modals/terms/terms.component.ts
+++ src/app/modals/terms/terms.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { CloseGuiService } from 'app/core/close-gui/close-gui.service';
 @Component({
@@ -6,10 +6,15 @@ import { CloseGuiService } from 'app/core/close-gui/close-gui.service';
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss']
 })
-export class TermsComponent {
+export class TermsComponent implements AfterViewInit {
   isScrolled: boolean = false;
+  @ViewChild('terms') terms: any;
   public text: string;
   constructor(private dialog: MatDialogRef<TermsComponent>, private close: CloseGuiService) { }
+
+  ngAfterViewInit() {
+    this.isScrolled = this.terms.nativeElement.clientHeight === this.terms.nativeElement.scrollHeight;
+  }
 
   acceptTerms(): void {
     this.dialog.close();
