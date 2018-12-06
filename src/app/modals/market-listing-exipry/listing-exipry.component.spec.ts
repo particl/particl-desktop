ff6a56acf97562908e4882b5122b4b2165ffc527
Shubham T <shubham.t@cisinlabs.com>
2018-12-06 17:04:13 +0530

    - fixed test cases.

--- src/app/modals/market-listing-exipry/listing-exipry.component.spec.ts
+++ src/app/modals/market-listing-exipry/listing-exipry.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SharedModule } from 'app/wallet/shared/shared.module';
+import { ModalsModule } from 'app/modals/modals.module';
+import { CoreModule } from 'app/core/core.module';
+import { CoreUiModule } from 'app/core-ui/core-ui.module';
+import { MatDialogRef } from '@angular/material';
+
 import { ListingExipryComponent } from './listing-exipry.component';
 
 describe('ListingExipryComponent', () => {
@@ -8,7 +14,15 @@ describe('ListingExipryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListingExipryComponent ]
+      imports: [
+        SharedModule,
+        ModalsModule,
+        CoreModule.forRoot(),
+        CoreUiModule.forRoot()
+      ],
+      providers: [
+        { provide: MatDialogRef }
+      ],
     })
     .compileComponents();
   }));
