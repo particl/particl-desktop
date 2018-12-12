985e863d95bfeac6c93029d2fc11883c3d3c4097
vikas <vikas.v@cisinlabs.com>
2018-12-12 18:51:21 +0530

    created rpc mock for transactions and written test cases

---
+++ src/app/core/rpc/rpc.responses.ts
@@ -0,0 +1,22 @@
+const Result = {
+  'filtertransactions' : [
+      {
+        'confirmations': 87,
+        'blockhash': '3369674d2bf9dd22a295051be04b96d2744eeea0ba6c19bbab056dff9e261edf',
+        'blockindex': 1,
+        'blocktime': 1517431088,
+        'txid': 'c8ee6c919e5b258f1e29e03c04baa2318b95ede030aa14a25cb9f060ade10cbd',
+        'time': 1517431047,
+        'timereceived': 1517431047,
+        'bip125_replaceable': 'no',
+        'fee': -0.00034800,
+        'category': 'internal_transfer',
+        'outputs': [
+        ],
+        'amount': 0.00000000
+      }
+    ]
+}
+export {
+  Result
+}
--- src/app/wallet/wallet/shared/transaction.service.spec.ts
+++ src/app/wallet/wallet/shared/transaction.service.spec.ts
@@ -1,25 +1,40 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SharedModule } from '../../shared/shared.module';
-import { RpcModule } from '../../../core/rpc/rpc.module';
+import { RpcService, RpcStateService } from '../../../core/rpc/rpc.module';
 import { CoreModule } from '../../../core/core.module';
-
+import { MockRpcService } from 'app/core/rpc/rpc.mockservice';
 import { TransactionService } from './transaction.service';
 
-
-describe('TransactionService', () => {
+fdescribe('TransactionService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         SharedModule,
-        RpcModule.forRoot(),
         CoreModule.forRoot()
       ],
-      providers: [TransactionService]
+      providers: [
+        RpcStateService,
+        TransactionService,
+        { provide: RpcService, useClass: MockRpcService }
+      ]
     });
   });
 
-  it('should ...', inject([TransactionService], (service: TransactionService) => {
+  it('should be create', inject([TransactionService], (service: TransactionService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should load transactions', inject([TransactionService], (service: TransactionService) => {
+    service.loadTransactions();
+    expect(service.txs.length).toBeGreaterThan(0);
+    expect(service.loading).toBe(false);
+  }));
+
+  // it('should filter transactions', inject([TransactionService], (service: TransactionService) => {
+  //   service.filter();
+  //   expect(service.txs.length).toBeGreaterThan(0);
+  //   expect(service.loading).toBe(false);
+  // }));
+
 });
