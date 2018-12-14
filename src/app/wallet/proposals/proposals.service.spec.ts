a6eed8cf1280f77a18c0aae9cafeec768e41c983
Shubham T <shubham.t@cisinlabs.com>
2018-12-14 20:40:32 +0530

    - Use the mock.market.service instead of market service.

--- src/app/wallet/proposals/proposals.service.spec.ts
+++ src/app/wallet/proposals/proposals.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProposalsService } from './proposals.service';
 import { RpcService } from 'app/core/rpc/rpc.service';
 import { CoreModule } from 'app/core/core.module';
+import { MarketService } from 'app/core/market/market.service';
+import { MockMarketService } from 'app/core/market/market.mockservice';
+import { ProfileService } from 'app/core/market/api/profile/profile.service';
+import { MarketStateService } from 'app/core/market/market-state/market-state.service';
+import { AddressService } from 'app/core/market/api/profile/address/address.service';
 
-describe('ProposalsService', () => {
+fdescribe('ProposalsService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        CoreModule.forRoot()
+        HttpClientModule
       ],
-      providers: [ProposalsService]
+      providers: [
+        {
+          provide: MarketService, useClass: MockMarketService
+        },
+        MarketStateService,
+        AddressService,
+        ProfileService,
+        ProposalsService,
+      ]
     });
   });
 
