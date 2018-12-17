c50ba92d4f6d634f5d68a3c1ae26ae49590df3d0
Shubham T <shubham.t@cisinlabs.com>
2018-12-17 18:30:18 +0530

    - Added added the mock response for the vote(optionId), vote get, vote result data in mock json file. - Created the vote related mock services.

---
+++ src/app/wallet/proposals/proposals.mockservice.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Responses } from 'app/core/market/mock-data/mock-market.responses';
+
+/*
+  This is a fake mock service used for the MockProposalsService.
+  so we have to override it in all tests that use the Services/ Components.
+*/
+
+@Injectable()
+export class MockProposalsService {
+
+  list(method: string, params?: Array<any> | null): Observable<any> {
+    const response = Responses['proposal']['list'];
+    return Observable.of(response);
+  }
+
+  post(options: Array<any> = []) {
+    const response = {};
+    return Observable.of(response);
+  }
+
+  result(proposalHash: string) {
+    const response = Responses['vote']['result'][proposalHash];
+    return Observable.of(response);
+  }
+
+  vote(options: Array<any>) {
+    let response = {};
+    if (options && options[0] && options[1]) {
+      response = Responses['vote'][options[0]][options[1]];
+    }
+    return Observable.of(response);
+  }
+
+  get(proposalHash: string) {
+    const response = (Responses['vote']['get'][proposalHash] || Responses['vote']['get'][404]);
+    return Observable.of(response);
+  }
+};
