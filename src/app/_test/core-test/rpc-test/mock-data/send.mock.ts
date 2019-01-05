760e91250e80913865f5ebc65a3bf690e6fcf081
vikas <vikas.v@cisinlabs.com>
2019-01-05 21:54:59 +0530

    mock service for send and changes accordingly

---
+++ src/app/_test/core-test/rpc-test/mock-data/send.mock.ts
@@ -0,0 +1,7 @@
+const mockSendInfo = 'e5eb869fda5350632bed3ef6a9c657d06d8555d46eae2e86a38a70bbf494b840';
+// tslint:disable-next-line
+const listStealth = 'Teta8T2KX5XQHN87JVUunNAkaX7F1Q5TgkB6QPGMMSaRYAF8oVesvgw5xxrJp1Pom2jKNFYJxKPAkzQgG87JwBRgNadFfTnBrgrEo6';
+export {
+  mockSendInfo,
+  listStealth
+}
--- src/app/wallet/wallet/send/send.service.spec.ts
+++ src/app/wallet/wallet/send/send.service.spec.ts
@@ -3,9 +3,9 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CoreUiModule } from '../../../core-ui/core-ui.module';
 import { CoreModule } from '../../../core/core.module';
 import { SharedModule } from '../../shared/shared.module'; // is this even needed?
-
+import { RpcService } from 'app/core/rpc/rpc.service';
 import { SendService } from './send.service';
-
+import { RpcMockService } from 'app/_test/core-test/rpc-test/rpc-mock.service';
 
 describe('SendService', () => {
   beforeEach(() => {
@@ -15,7 +15,10 @@ describe('SendService', () => {
         CoreModule.forRoot(),
         CoreUiModule.forRoot()
       ],
-      providers: [SendService]
+      providers: [
+      SendService,
+      { provide: RpcService, useClass: RpcMockService }
+      ]
 
     });
   });
