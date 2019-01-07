c42ba0ce1eeeb87ec1fd3d08e70c0fdce32ecaaa
Shubham T <shubham.t@cisinlabs.com>
2019-01-07 14:32:01 +0530

    Added the mock data for the information mock service.

---
+++ src/app/_test/core-test/market-test/template-test/information-test/mock-data/index.ts
@@ -0,0 +1,1 @@
+export { updateData } from './update';
---
+++ src/app/_test/core-test/market-test/template-test/information-test/mock-data/update.ts
@@ -0,0 +1,39 @@
+const updateData = {
+  'id': 1,
+  'jsonrpc': '2.0',
+  'result': {
+    'id': 5,
+    'title': 'listing test for escrow',
+    'shortDescription': 'listing added for esdsss',
+    'longDescription': 'esdsssss',
+    'itemCategoryId': 3,
+    'listingItemId': null,
+    'listingItemTemplateId': 1,
+    'updatedAt': 1546847792918,
+    'createdAt': 1546680317725,
+    'ShippingDestinations': [],
+    'ItemImages': [],
+    'ItemCategory': {
+      'id': 3,
+      'key': 'cat_particl_free_swag',
+      'name': 'Free Swag',
+      'description': '',
+      'parentItemCategoryId': 2,
+      'updatedAt': 1546847690013,
+      'createdAt': 1546597659889
+    },
+    'ItemLocation': {
+      'id': 5,
+      'region': 'AS',
+      'address': 'a',
+      'itemInformationId': 5,
+      'updatedAt': 1546680317883,
+      'createdAt': 1546680317883,
+      'LocationMarker': {}
+    }
+  }
+}
+
+export {
+  updateData
+}
