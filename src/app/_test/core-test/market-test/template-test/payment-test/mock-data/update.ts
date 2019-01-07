e5c71f4eb82a53f3fcec378064e567ffe667b47a
Shubham T <shubham.t@cisinlabs.com>
2019-01-07 14:34:01 +0530

    Added the payment service mock data.

---
+++ src/app/_test/core-test/market-test/template-test/payment-test/mock-data/update.ts
@@ -0,0 +1,44 @@
+const updateData = {
+  'id': 5,
+  'type': 'SALE',
+  'listingItemId': null,
+  'listingItemTemplateId': 1,
+  'updatedAt': 1546847793049,
+  'createdAt': 1546680317754,
+  'Escrow': {
+    'id': 5,
+    'type': 'MAD',
+    'paymentInformationId': 5,
+    'updatedAt': 1546847792965,
+    'createdAt': 1546680317946,
+    'Ratio': {
+      'id': 7,
+      'buyer': 100,
+      'seller': 100,
+      'escrowId': 5,
+      'updatedAt': 1546847792980,
+      'createdAt': 1546847792980
+    }
+  },
+  'ItemPrice': {
+    'id': 7,
+    'currency': 'PARTICL',
+    'basePrice': 1,
+    'paymentInformationId': 5,
+    'cryptocurrencyAddressId': null,
+    'updatedAt': 1546847793072,
+    'createdAt': 1546847793072,
+    'ShippingPrice': {
+      'id': 7,
+      'domestic': 12,
+      'international': 1,
+      'itemPriceId': 7,
+      'updatedAt': 1546847793082,
+      'createdAt': 1546847793082
+    }
+  }
+}
+
+export {
+  updateData
+}
