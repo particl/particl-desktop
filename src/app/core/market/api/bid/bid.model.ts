a14bdb0c7258f4d816670de29595fb9f8796b5b9
Jason Eybers <jasonheybers@gmail.com>
2019-01-08 10:11:46 +0200

    fix bid send

--- src/app/core/market/api/bid/bid.model.ts
+++ src/app/core/market/api/bid/bid.model.ts
@@ -69,7 +69,7 @@ export class Bid extends Product {
     if (this.order.ListingItem && this.order.ListingItem.hash) {
       return this.order.ListingItem.hash.substring(0, 4);
     }
-    return undefined;
+    return '';
   }
 
   get listing(): Listing {
