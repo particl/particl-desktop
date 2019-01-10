723e2c7bee05cac27b00735d4b94dbb883b79e80
Shubham T <shubham.t@cisinlabs.com>
2019-01-10 16:30:13 +0530

    - Write test cases using the location mock service later change with the market mock service.

--- src/app/core/market/api/template/location/location.service.spec.ts
+++ src/app/core/market/api/template/location/location.service.spec.ts
@@ -3,18 +3,41 @@ import { TestBed, inject } from '@angular/core/testing';
 import { MarketModule } from '../../../market.module';
 
 import { LocationService } from './location.service';
+import { LocationMockService } from 'app/_test/core-test/market-test/template-test/location-test/location-mock.service';
+import { CountryListService } from 'app/core/market/api/countrylist/countrylist.service';
+import { executeData } from 'app/_test/core-test/market-test/template-test/location-test/mock-data/execute';
 
 describe('LocationService', () => {
+  const templateId = 1;
+  const countryCode = 'IN'
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         MarketModule.forRoot()
       ],
-      providers: [LocationService]
+      providers: [
+        { provide: LocationService, useClass: LocationMockService }
+      ]
     });
   });
 
   it('should be created', inject([LocationService], (service: LocationService) => {
     expect(service).toBeTruthy();
   }));
+
+  it(
+    'should execute return the success and response data',
+    inject([LocationService, CountryListService], async (service: LocationService, countryService: CountryListService) => {
+
+      expect(service).toBeTruthy();
+      expect(countryService).toBeTruthy();
+
+      const country = countryService.getCountryByRegion(countryCode);
+
+      expect(country).not.toBe(null);
+
+      const response = await service.execute('add', templateId, country, null, null).toPromise()
+      expect(response).toEqual(executeData);
+    }));
 });
