81d3996616118fa1c8eb9dd5765a1fcd0914a52b
EagleEye25 <jasonheybers@gmail.com>
2019-01-14 11:41:04 +0200

    fix tests

--- src/app/modals/processing-modal/processing-modal.component.spec.ts
+++ src/app/modals/processing-modal/processing-modal.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProcessingModalComponent } from './processing-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'app/wallet/shared/shared.module';
+import { CoreModule } from 'app/core/core.module';
+import { CoreUiModule } from 'app/core-ui/core-ui.module';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 describe('ProcessingModalComponent', () => {
   let component: ProcessingModalComponent;
@@ -8,7 +13,17 @@ describe('ProcessingModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProcessingModalComponent ]
+      declarations: [ ProcessingModalComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        SharedModule,
+        CoreModule.forRoot(),
+        CoreUiModule.forRoot(),
+      ],
+      providers: [
+        { provide: MatDialogRef },
+        { provide: MAT_DIALOG_DATA }
+      ]
     })
     .compileComponents();
   }));
