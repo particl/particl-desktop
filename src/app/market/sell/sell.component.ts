c00aa603e75d484a814071c8ae0df0e979ead2d6
Kewde <kewde@particl.io>
2019-02-03 00:46:12 +0000

    lint

--- src/app/market/sell/sell.component.ts
+++ src/app/market/sell/sell.component.ts
@@ -215,7 +215,7 @@ export class SellComponent implements OnInit {
     const currentMaxPerPage = this.pagination.maxPerPage;
     const newMaxPerPage = window.innerWidth > 1330 ? 20 : 10;
     const isLarger = (newMaxPerPage - currentMaxPerPage) > 0;
-    
+
     if (isLarger) {
       // Load more pages to fill the screen
       // maxPages 2 -> 3, ensure no pages are deleted when loading
