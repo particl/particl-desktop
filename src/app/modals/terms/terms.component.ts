ebe73a08e14614de7a4079ae6b881690ed2a468f
vikas <vikas.v@cisinlabs.com>
2019-02-08 15:53:22 +0530

    reedit the accept terms functionality and removed unused code

--- src/app/modals/terms/terms.component.ts
+++ src/app/modals/terms/terms.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ChangeDetectorRef, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { CloseGuiService } from 'app/core/close-gui/close-gui.service';
 @Component({
@@ -6,22 +6,14 @@ import { CloseGuiService } from 'app/core/close-gui/close-gui.service';
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss']
 })
-export class TermsComponent implements AfterViewInit {
-  isScrolled: boolean = false;
+export class TermsComponent {
+  isAccepted: boolean = false;
 
-  @ViewChild('terms') terms: any;
   public text: string;
   constructor(
     private dialog: MatDialogRef<TermsComponent>,
-    private close: CloseGuiService,
-    private cd: ChangeDetectorRef
+    private close: CloseGuiService
   ) { }
-
-  ngAfterViewInit() {
-    this.isScrolled = this.terms.nativeElement.clientHeight > (this.terms.nativeElement.scrollHeight - 10);
-    // Fixing the change detection for test cases
-    this.cd.detectChanges();
-  }
 
   acceptTerms(): void {
     this.dialog.close();
@@ -29,29 +21,6 @@ export class TermsComponent implements AfterViewInit {
 
   decline(): void {
     this.close.quitElectron();
-  }
-
-  onScroll($event: any) {
-    const pos = $event.target.offsetHeight + $event.target.scrollTop;
-    const max = $event.target.scrollHeight;
-    if (pos === max) {
-      this.isScrolled = true;
-    }
-  }
-
-  scrollbarVisible() {
-    if (this.terms.nativeElement.scrollHeight > this.terms.nativeElement.clientHeight) {
-      this.isScrolled = false;
-      return false;
-    } else {
-      this.isScrolled = true;
-      return true;
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-    onResize() {
-      this.scrollbarVisible();
   }
 
 }
