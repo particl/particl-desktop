65f12067e6dcfd6ba639c05cdb3c84882547c4ba
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2019-03-07 12:55:56 +0200

    Ensures the daemon download manager cleans up if necessary after a download (removing invalid listener).

--- modules/init.js
+++ modules/init.js
@@ -55,6 +55,7 @@ daemonManager.on('status', (status, msg) => {
   // Done -> means we have a binary!
   if (status === 'done') {
     log.debug('daemonManager returned successfully, starting daemon!');
+    daemonManager.shutdown();
     multiwallet.get()
     // TODO: activate for prompting wallet
     .then(chosenWallets => {
