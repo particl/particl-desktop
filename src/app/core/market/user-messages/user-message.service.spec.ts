990ce8f9a352715c4f6ba6e78db6809e37b3d8c7
Shubham T <shubham.t@cisinlabs.com>
2019-03-15 19:38:28 +0530

    - Write the test cases for the user message service.

--- src/app/core/market/user-messages/user-message.service.spec.ts
+++ src/app/core/market/user-messages/user-message.service.spec.ts
@@ -1,10 +1,21 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from 'app/core/core.module';
 
 import { UserMessageService } from './user-message.service';
+import { UserMessageType, UserMessage } from './user-message.model';
+import { AlphaMainnetWarningComponent } from 'app/modals/alpha-mainnet-warning/alpha-mainnet-warning.component';
 
-describe('ProposalsNotificationsService', () => {
+describe('UserMessageService', () => {
+  const alphaMessage = {
+    text: 'The Particl Marketplace alpha is still in development and not 100% private yet - use it at your own risk!',
+    dismissable: false,
+    timeout: 0,
+    messageType: UserMessageType.ALERT,
+    action: () => { this.dialog.open(AlphaMainnetWarningComponent); },
+    actionLabel: 'Click here to read all the details first!'
+  } as UserMessage;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -20,4 +31,25 @@ describe('ProposalsNotificationsService', () => {
   it('should be created', inject([UserMessageService], (service: UserMessageService) => {
     expect(service).toBeTruthy();
   }));
+
+  it(
+    'should be addMessage method emit the message where it is subscribed',
+    fakeAsync(
+      inject([UserMessageService], (service: UserMessageService) => {
+        expect(service).toBeTruthy();
+        let message = null;
+        service.message.subscribe((m) => {
+          message = m;
+        });
+
+        expect(message).toBe(null);
+
+        service.addMessage(alphaMessage);
+        tick(1000);
+
+        expect(message).not.toBe(null);
+        expect(message).toEqual(alphaMessage);
+      })
+    )
+  );
 });
