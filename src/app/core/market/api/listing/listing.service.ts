0ad0484c4f0fac85a7e0c478161c32ad188c5cf9
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2019-03-15 10:03:49 +0200

    Removes unnecessary console.log statement.

--- src/app/core/market/api/listing/listing.service.ts
+++ src/app/core/market/api/listing/listing.service.ts
@@ -38,7 +38,6 @@ export class ListingService {
     return this.market.call('item', params)
     .map(
       (listings: Array<Listing>) => {
-        console.log('listings.map(t => new Listing(t))----', listings.map(t => new Listing(t)));
         return listings.map(t => new Listing(t));
       }
     ).do(
