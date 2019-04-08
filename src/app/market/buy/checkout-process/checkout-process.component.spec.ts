1dbb974da4b78c4b722ba6c2a9c9b28bc6ec6f7c
vikas <vikas.v@cisinlabs.com>
2019-04-08 19:53:41 +0530

    console and checkout comp test fixes

--- src/app/market/buy/checkout-process/checkout-process.component.spec.ts
+++ src/app/market/buy/checkout-process/checkout-process.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CoreModule } from 'app/core/core.module';
 import { CoreUiModule } from 'app/core-ui/core-ui.module';
@@ -18,6 +18,7 @@ describe('CheckoutProcessComponent', () => {
     TestBed.configureTestingModule({
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       imports: [
+        BrowserAnimationsModule,
         RouterTestingModule,
         HttpClientTestingModule,
         CoreModule.forRoot(),
