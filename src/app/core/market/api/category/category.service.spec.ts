fad15a23c6bd62884f2b27581d58d33b3b7ffe69
vikas <vikas.v@cisinlabs.com>
2019-04-08 15:23:07 +0530

    category service test case fixed

--- src/app/core/market/api/category/category.service.spec.ts
+++ src/app/core/market/api/category/category.service.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MarketModule } from '../../market.module';
-
+import { CoreModule } from 'app/core/core.module';
 import { CategoryService } from './category.service';
 import { MarketService } from '../../market.service';
 
-describe('CategoryService', () => {
+fdescribe('CategoryService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
+        HttpClientModule,
+        CoreModule.forRoot(),
         MarketModule.forRoot()
       ],
       providers: [CategoryService, MarketService]
