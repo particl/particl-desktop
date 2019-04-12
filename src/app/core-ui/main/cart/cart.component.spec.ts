e7b0cb2f81d6155edcf00145791b67000f094bd9
Shubham T <shubham.t@cisinlabs.com>
2019-04-12 19:24:19 +0530

    Writer the test cases for cart component

--- src/app/core-ui/main/cart/cart.component.spec.ts
+++ src/app/core-ui/main/cart/cart.component.spec.ts
@@ -5,11 +5,13 @@ import { CoreModule } from '../../../core/core.module';
 import { CoreUiModule } from '../../core-ui.module';
 
 import { CartComponent } from './cart.component';
+import { cartItemList } from 'app/_test/core-test/market-test/api-test/cart-test/mock-data';
+import { Cart } from 'app/core/market/api/cart/cart.model';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
-
+  const cart = cartItemList;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -29,6 +31,49 @@ describe('CartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should cart cart info section exist', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const cartInfo = compiled.querySelector('.cart-info');
+    expect(cartInfo).toBeTruthy();
+  });
+
+  it('should cart icon exist in cart info section', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const icons = compiled.querySelector('.cart-info mat-icon.part-cart');
+    expect(icons).toBeTruthy();
+  });
+
+  it('should amount section exist if cart count is greater than 0', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const itemCountSection = compiled.querySelector('.cart-info .tag');
+    expect(itemCountSection).toBeTruthy();
+    const itemCount = +itemCountSection.textContent;
+    const amountSec = compiled.querySelector('.cart-info .amount');
+
+    if (itemCount) {
+      expect(amountSec).toBeTruthy();
+    } else {
+      expect(amountSec).toBeFalsy();
+    }
+  });
+
+  it('should amount section exist if cart count is greater than 0', () => {
+    component.cart = new Cart(cart);
+
+    // detect the changes.
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    const itemCountSection = compiled.querySelector('.cart-info .tag');
+    expect(itemCountSection).toBeTruthy();
+    const itemCount = +itemCountSection.textContent;
+    const amountSec = compiled.querySelector('.cart-info .amount');
+
+    expect(itemCount).not.toEqual(0);
+
+    expect(amountSec).toBeTruthy();
   });
 
 });
