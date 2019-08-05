50a77021970534fc3a5dd694c8fee27c4b35d194
Robert Edwards <robertedwardsmail@gmail.com>
2019-08-05 11:16:00 +0200

    Identify comment sender

--- src/app/market/shared/comments/comment/comment.component.ts
+++ src/app/market/shared/comments/comment/comment.component.ts
@@ -21,6 +21,22 @@ export class CommentComponent {
   @Input() sellerAddress: string;
   @Output() replied: EventEmitter<any> = new EventEmitter<any>();
 
+  get senderDetail(): string {
+    if (this.comment && this.comment.sender) {
+      var hash = 0;
+      for (var i = 0; i < this.comment.sender.length; i++) {
+        hash = this.comment.sender.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      var colour = '#';
+      for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+      }
+      return colour;
+    }
+    return '';
+  }
+
   constructor(
     private commentService: CommentService,
     private modals: ModalsHelperService,
