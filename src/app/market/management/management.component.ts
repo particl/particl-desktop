70711bc1013b03c9fe78d252e6d1f5db81142a4d
Martin Allien <martin@allien.work>
2019-08-19 15:13:16 +0200

    Management: sort/filter sidebar layout

--- src/app/market/management/management.component.ts
+++ src/app/market/management/management.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./management.component.scss']
 })
 export class ManagementComponent implements OnInit {
+  public isLoading: boolean = false;
+  public isPageLoading: boolean = false;
 
   public selectedTab: number = 0;
   public tabLabels: Array<string> = ['available', 'joined'];
+
+  markets_sortings: Array<any> = [
+    { title: 'By title', value: 'TITLE' },
+    { title: 'By ??', value: '??' }
+  ];
+
+  markets_filtering: Array<any> = [
+    { title: 'All markets',       value: '' },
+    { title: 'Community Markets', value: 'community' },
+    { title: 'Storefronts',       value: 'storefronts' }
+  ];
 
   constructor() { }
 
