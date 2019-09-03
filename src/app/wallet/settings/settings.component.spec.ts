26a8aa579147a465c7adbb83525c09c6f01b9c93
Martin Allien <martin@allien.work>
2019-09-03 12:04:48 +0200

    Settings (v2): page basics

--- src/app/wallet/settings/settings.component.spec.ts
+++ src/app/wallet/settings/settings.component.spec.ts
@@ -1,10 +1,4 @@
-import { Location } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-
-import { SharedModule } from '../shared/shared.module';
-
-import { SettingsService } from './settings.service';
 
 import { SettingsComponent } from './settings.component';
 
@@ -14,9 +8,7 @@ describe('SettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, SharedModule ],
-      declarations: [ SettingsComponent ],
-      providers: [ SettingsService, Location ]
+      declarations: [ SettingsComponent ]
     })
     .compileComponents();
   }));
@@ -29,33 +21,5 @@ describe('SettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-/*
-  it('should go to settingsTab', () => {
-    // component.settingsTab();
-    expect(component.settings).toBeTruthy();
-  });
-
-  it('should applySettings', () => {
-    component.apply();
-    expect(component.apply).toBeTruthy();
-  });
-
-  it('should cancel', () => {
-    component.cancel();
-    expect(component.cancel).toBeTruthy();
-  });
-
-  it('should validate', () => {
-    component.validate();
-    expect(component.validate).toBeTruthy();
-  });
-*/
-  it('should get settings', () => {
-    expect(component.settings).toBeDefined();
-  });
-
-  it('should get tab', () => {
-    expect(component.tab).toBe('main');
   });
 });
--- src/app/wallet/settings/settings.component.ts
+++ src/app/wallet/settings/settings.component.ts
@@ -1,55 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-
-import { SettingsService } from './settings.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss'],
-  providers: [
-    SettingsService
-  ]
+  styleUrls: ['./settings.component.scss']
 })
-
 export class SettingsComponent implements OnInit {
 
-  tab: string = 'main';
-  settings: Object;
+  public selectedTab: number = 0;
+  public tabLabels: Array<string> = ['one', 'two', 'three']; // FIXME: remove sell_item and leave as a separate page?
 
-  constructor(
-    private _settingsService: SettingsService,
-    private _location: Location
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    /* Preload default settings if none found */
-    if (localStorage.getItem('settings') == null) {
-      const settings: string = JSON.stringify(this._settingsService.defaultSettings);
-      localStorage.setItem('settings', settings);
-    }
-    this.settings = this._settingsService.loadSettings();
-  }
-
-  settingsTab(tab: string) {
-    this.tab = tab;
-    if (tab === 'help') {
-      // const dialogRef = this.dialog.open(ConsoleModalComponent);
-    }
-  }
-
-  apply() {
-    this._settingsService.applySettings(this.settings);
-  }
-
-  cancel() {
-    this.settings = this._settingsService.loadSettings();
-    this._location.back();
-  }
-
-  validate() {
-    this.apply();
-    this._location.back();
   }
 
 }
