487e8464ba1ecacfc1fce446fe18a9f87f20db19
Robert Edwards <robertedwardsmail@gmail.com>
2019-09-10 15:27:49 +0200

    Extract list of categories

--- src/app/market/sell/import-listings/import-custom-ui/csv/csv.component.ts
+++ src/app/market/sell/import-listings/import-custom-ui/csv/csv.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CategoryService } from 'app/core/market/api/category/category.service';
+import { take } from 'rxjs/operators';
 
 
 @Component({
@@ -10,7 +12,31 @@ export class ImportCustomUiCsvComponent {
   @Input() import: any;
   @Output() clear: EventEmitter<any> = new EventEmitter<any>();
 
+  constructor(
+    private category: CategoryService
+  ) {}
+
   emitClear() {
     this.clear.emit();
+  }
+
+  generateCategories() {
+
+    this.category.list().pipe(
+      take(1)
+    ).subscribe(rootCategory => {
+      let csv = 'Parent Category (Not selectable), Child Category\n';
+      for (const parentCategory of rootCategory.subCategoryList) {
+        for (const childCategory of parentCategory.subCategoryList) {
+          csv += `${parentCategory.name},${childCategory.name}\n`;
+        }
+      }
+      const csvData = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
+      const csvURL = window.URL.createObjectURL(csvData);
+      const tempLink = document.createElement('a');
+      tempLink.href = csvURL;
+      tempLink.setAttribute('download', 'categories.csv');
+      tempLink.click();
+    });
   }
 }
