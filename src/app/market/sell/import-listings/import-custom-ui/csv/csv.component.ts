ed2a806782b8d269429a02b729754a664aa64158
Robert Edwards <robertedwardsmail@gmail.com>
2019-09-11 09:28:45 +0200

    Remove category extract

--- src/app/market/sell/import-listings/import-custom-ui/csv/csv.component.ts
+++ src/app/market/sell/import-listings/import-custom-ui/csv/csv.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { CategoryService } from 'app/core/market/api/category/category.service';
-import { take } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-import-custom-ui-csv',
@@ -12,31 +9,7 @@ export class ImportCustomUiCsvComponent {
   @Input() import: any;
   @Output() clear: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(
-    private category: CategoryService
-  ) {}
-
   emitClear() {
     this.clear.emit();
-  }
-
-  generateCategories() {
-
-    this.category.list().pipe(
-      take(1)
-    ).subscribe(rootCategory => {
-      let csv = 'Parent Category (Not selectable), Child Category\n';
-      for (const parentCategory of rootCategory.subCategoryList) {
-        for (const childCategory of parentCategory.subCategoryList) {
-          csv += `${parentCategory.name},${childCategory.name}\n`;
-        }
-      }
-      const csvData = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
-      const csvURL = window.URL.createObjectURL(csvData);
-      const tempLink = document.createElement('a');
-      tempLink.href = csvURL;
-      tempLink.setAttribute('download', 'categories.csv');
-      tempLink.click();
-    });
   }
 }
