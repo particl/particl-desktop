bbf458a9441cb30f86c38286f8010265c0c5a679
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2019-09-20 07:59:59 +0200

    [WIP] settings page implementation, save point...

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,42 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import { RpcService } from 'app/core/rpc/rpc.service';
 import { MatDialog } from '@angular/material';
 
+import { range } from 'lodash';
+
 import { ProcessingModalComponent } from 'app/modals/processing-modal/processing-modal.component';
 
 enum SettingType {
   STRING = 0,
   NUMBER = 1,
   BOOLEAN = 2,
-  SELECT = 3
+  SELECT = 3,
+  BUTTON = 4
 };
 
 class SelectableOption {
   text: string;
   value: any;
-  isCurrent: boolean;
   isDisabled: boolean;
+}
+
+class ParamsNumber {
+  min: number;
+  max: number;
+  step: number;
+}
+
+class ParamsString {
+  placeholder: string;
+}
+
+class ParamsButton {
+  icon: string;
+  color: string;
 }
 
 class Setting {
   title: string;
   description: string;
   isDisabled: boolean;
-  isButton: boolean;
   type: SettingType;
+  limits: null | ParamsNumber | ParamsString | ParamsButton;
   errorMsg?: string;
-  options?: SelectableOption[];  // Only applicable if type === SettingType.SELECT
-  currentValue: any;
-  newValue: any;
+  options?: SelectableOption[];   // Only applicable if type === SettingType.SELECT
+  currentValue: any;              // Ignored for SettingType.BUTTON
+  newValue: any;                  // Ignored for SettingType.BUTTON
   tags: string[];
   restartRequired: boolean;
   validate?: Function;
   onChange?: Function;
-  onAction?: Function;
+  fn?: Function;            // Currently only applicable for SettingType.BUTTON
 };
 
 class SettingGroup {
   name: string;
-  description: string;
   settings: Setting[];
 };
 
@@ -48,12 +64,12 @@ class PageInfo {
 };
 
 class Page {
-  header: string;
-  info: PageInfo;
-  icon: string;
-  title: string;
-  settingGroups: SettingGroup[];
-  isChanged: boolean;
+  header: string;                 // tab name
+  icon: string;                   // tab icon
+  info: PageInfo;                 // general information about the page (tab)
+  settingGroups: SettingGroup[];  // a group of similar settings... all displayed close to each other
+  isChanged: boolean;             // indicates that a setting has been changed (UI control use)
+  load: Function;                 // executed when the page (tab) is loaded
 };
 
 enum ProcessingMessages {
@@ -68,11 +84,19 @@ enum ProcessingMessages {
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
-  public pageIdx: number = -1;
   public settingPages: Array<Page> = [];
   public isLoading: boolean = true;
+
+  public settingType: (typeof SettingType) = SettingType;
+
+  // Convenience properties for assisting the laying out columns of setting groups
+  public columnCount: number = 2;
+  public colIdxs: number[] = range(0, this.columnCount);
+  public groupIdxs: number[] = [];
+
+  // Internal (private) vars
   private isProcessing: boolean = false;
+  private pageIdx: number = -1;
 
   constructor(
     private _rpc: RpcService,
@@ -81,6 +105,7 @@ export class SettingsComponent implements OnInit {
   };
 
   ngOnInit() {
+    // Create the pages (tabs) used here
     const walletName = this._rpc.wallet === '' ? 'Default' : this._rpc.wallet;
     const walletSettings = {
       header: 'Wallet Settings',
@@ -90,7 +115,9 @@ export class SettingsComponent implements OnInit {
         description: 'Adjust settings and configuration for the currently active wallet',
         help: 'To change settings for other wallets, switch to them first in the multiwallet sidebar and visit this page again.'
       } as PageInfo,
-      settingGroups: []
+      settingGroups: [],
+      isChanged: false,
+      load: this.loadWalletSettings,
     } as Page;
     const globalSettings = {
       header: 'Global Settings',
@@ -100,7 +127,9 @@ export class SettingsComponent implements OnInit {
         description: 'Adjust settings and configuration that apply to the application (rather than an individual wallet)',
         help: 'Please take note of setting changes that require a service restart.'
       } as PageInfo,
-      settingGroups: []
+      settingGroups: [],
+      isChanged: false,
+      load: this.loadGlobalSettings,
     } as Page;
 
     this.settingPages.push(walletSettings);
@@ -108,10 +137,7 @@ export class SettingsComponent implements OnInit {
 
     setTimeout(() => {
       this.disableUI(ProcessingMessages.LOADING);
-      this.pageIdx = 0;
-      this.loadTabData();
-      this.isLoading = false;
-      this.enableUI();
+      this.changeTab(0);
     }, 0);
   }
 
@@ -128,11 +154,9 @@ export class SettingsComponent implements OnInit {
     }
     this.isLoading = true;
     this.disableUI(ProcessingMessages.LOADING);
-    this.clearCurrentChanges();
     this.pageIdx = idx;
-    this.loadTabData();
-    this.isLoading = false;
-    this.enableUI();
+    // this.resetPageChanges(idx);  // Perform this step only if NOT requesting settings on page load
+    this.loadPageSettings(idx);
   }
 
   clearChanges() {
@@ -141,36 +165,50 @@ export class SettingsComponent implements OnInit {
     }
     this.isProcessing = true;
     this.disableUI(ProcessingMessages.RESET);
-    this.clearCurrentChanges();
+    this.resetPageChanges(this.pageIdx);
     this.isProcessing = false;
     this.enableUI();
   }
 
-  private clearCurrentChanges() {
-    // Reset each page's settings to the previous saved value;
-    this.currentPage.settingGroups.forEach(group => {
+  private resetPageChanges(pageIndex: number) {
+    // Reset each setting for the indicated page to the last saved value;
+    this.settingPages[pageIndex].settingGroups.forEach(group => {
       group.settings.forEach(setting => {
-        if(!(setting.isDisabled || setting.isButton) ) {
-          if (setting.type === SettingType.SELECT) {
-            if (setting.options && setting.options.length) {
-              const defaultIdx = setting.options.findIndex(option => option.isCurrent);
-              if ( defaultIdx > -1) {
-                setting.newValue = setting.options[defaultIdx].value;
-              }
-            }
-          } else {
-            setting.newValue = setting.currentValue;
-          }
+        if ( !(setting.type === SettingType.BUTTON)) {
+          setting.newValue = setting.currentValue;
         }
         setting.errorMsg = '';
       })
     });
-    this.currentPage.isChanged = false;
+    this.settingPages[pageIndex].isChanged = false;
   }
 
-  private loadTabData() {
-    // Load the current values for each of the current tab data's.
+  private async loadPageSettings(pageIndex: number) {
+    const selectedPage = this.settingPages[pageIndex];
 
+    if (selectedPage.settingGroups.length) {
+      this.resetPageChanges(pageIndex);
+      this.groupIdxs = range(0, selectedPage.settingGroups.length, this.columnCount);
+      this.isLoading = false;
+      this.enableUI();
+      return;
+    }
+
+    this.groupIdxs = [];
+
+    let fn: Function;
+    if (selectedPage && (typeof selectedPage.load === 'function')) {
+      fn = selectedPage.load.bind(this);
+    }
+
+    if (fn !== undefined) {
+      await fn(selectedPage).catch(() => {}).then(() => {
+        this.resetPageChanges(pageIndex);
+        this.groupIdxs = range(0, selectedPage.settingGroups.length, this.columnCount);
+        this.isLoading = false;
+        this.enableUI();
+      });
+    }
   }
 
   private disableUI(message: string) {
@@ -184,5 +222,164 @@ export class SettingsComponent implements OnInit {
 
   private enableUI() {
     this._dialog.closeAll();
+  }
+
+  private async loadWalletSettings() {
+    const settingGroups: SettingGroup[] = [];
+    const group1 = {
+      name: 'Test Group',
+      settings: []
+    } as SettingGroup;
+
+    const group2 = {
+      name: 'Test Group 2',
+      settings: []
+    } as SettingGroup;
+
+    const group3 = {
+      name: 'Test Group 3',
+      settings: []
+    } as SettingGroup;
+
+    const group4 = {
+      name: 'Test Group 4',
+      settings: []
+    } as SettingGroup;
+
+    const group5 = {
+      name: 'Test Group 5',
+      settings: []
+    } as SettingGroup;
+
+    const testNum = {
+      title: 'testing Number input',
+      description: 'number description goes here',
+      isDisabled: true,
+      type: SettingType.NUMBER,
+      errorMsg: '',
+      currentValue: 12,
+      tags: ['tag1', 'tag2'],
+      restartRequired: false
+    } as Setting;
+
+    const testNum2 = {
+      title: 'testing other input',
+      description: 'number description goes here',
+      isDisabled: false,
+      type: SettingType.NUMBER,
+      errorMsg: '',
+      currentValue: 12,
+      tags: ['tag1', 'tag2'],
+      restartRequired: true,
+      limits: { min: 5, max: 50, step: 5} as ParamsNumber
+    } as Setting;
+
+    const testSelect = {
+      title: 'testing Selectable input',
+      description: 'select with some options description goes here',
+      isDisabled: false,
+      type: SettingType.SELECT,
+      options: [
+        {text: 'option 1', value: 1, isDisabled: false},
+        {text: 'option 2', value: 2, isDisabled: true},
+        {text: 'option 3', value: 3, isDisabled: false},
+      ],
+      errorMsg: '',
+      currentValue: 1,
+      tags: ['tag1'],
+      restartRequired: true
+    } as Setting;
+
+    const testString = {
+      title: 'testing input type input',
+      description: 'regular text description goes here',
+      isDisabled: false,
+      type: SettingType.STRING,
+      errorMsg: 'example error message',
+      currentValue: 'some value',
+      tags: [],
+      restartRequired: true
+    } as Setting;
+
+    const testString2 = {
+      title: 'testing 2nd input type input',
+      description: 'regular text description goes here',
+      isDisabled: true,
+      type: SettingType.STRING,
+      errorMsg: '',
+      currentValue: 'another value',
+      tags: ['tag', 'tag2'],
+      restartRequired: false
+    } as Setting;
+
+    const testButton = {
+      title: 'test a button here',
+      description: 'does the button text even count',
+      isDisabled: true,
+      type: SettingType.BUTTON,
+      errorMsg: '',
+      tags: [],
+      restartRequired: true
+    } as Setting;
+
+    const testButton2 = {
+      title: 'test a button here',
+      description: '',
+      isDisabled: false,
+      type: SettingType.BUTTON,
+      errorMsg: 'example error message',
+      tags: [],
+      restartRequired: true,
+      limits: { icon: 'part-check', color: 'primary' }
+    } as Setting;
+
+    const testString3 = {
+      title: 'testing input type input',
+      description: 'regular text description goes here',
+      isDisabled: false,
+      type: SettingType.STRING,
+      errorMsg: '',
+      currentValue: 'some value',
+      tags: [],
+      restartRequired: true,
+      limits: { placeholder: 'test string 3 input' }
+    } as Setting;
+
+    const testBool1 = {
+      title: 'testing checkboxes',
+      description: 'a checkbox test',
+      isDisabled: true,
+      type: SettingType.BOOLEAN,
+      errorMsg: 'example error message',
+      currentValue: true,
+      tags: ['yay'],
+      restartRequired: true,
+    } as Setting;
+
+    group1.settings.push(testNum);
+    group1.settings.push(testSelect);
+    group1.settings.push(testString);
+    group1.settings.push(testBool1);
+
+    group2.settings.push(testString2);
+    group2.settings.push(testButton);
+
+    group3.settings.push(testString3);
+
+    group4.settings.push(testButton2);
+
+    group5.settings.push(testNum2);
+
+    settingGroups.push(group1);
+    settingGroups.push(group2);
+    settingGroups.push(group3);
+    settingGroups.push(group4);
+    settingGroups.push(group5);
+
+    this.settingPages[this.pageIdx].settingGroups = settingGroups;
+  }
+
+  private async loadGlobalSettings() {
+    console.log('@@@@ loading global settings');
   }
 }
