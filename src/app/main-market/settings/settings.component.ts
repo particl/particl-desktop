9a6759384ddd581eaf64d7977b49e1a34b716cea
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2020-03-18 14:14:26 +0200

    Adds banner into market settings component to indicate the start status of particl market service (and provide a restart action when approrpriate).

--- src/app/main-market/settings/settings.component.ts
+++ src/app/main-market/settings/settings.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { MarketState } from '../store/market.state';
+import { StartedStatus } from '../store/market.models';
+import { Subject } from 'rxjs';
+import { tap, takeUntil } from 'rxjs/operators';
+import { MarketActions } from '../store/market.actions';
 
 
 @Component({
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class MarketSettingsComponent {
+export class MarketSettingsComponent implements OnDestroy {
+
+  startedStatus: StartedStatus;
+  StartedStatus: typeof StartedStatus = StartedStatus;
+
+
+  private destroy$: Subject<void> = new Subject();
+
+  constructor(
+    private _store: Store
+  ) {
+    this._store.select(MarketState.startedStatus).pipe(
+      tap((status) => this.startedStatus = status),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  restartMarketplace() {
+    this._store.dispatch(new MarketActions.StartMarketService());
+  }
 }
