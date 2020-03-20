40b9250b6dfa84af7f230603c3d12b1f960bc48a
Martin Allien <martin@allien.work>
2020-03-20 16:57:06 +0100

    Sell: basic layout + input w/ icon tweaks

--- src/app/main-market/sell/sell.component.ts
+++ src/app/main-market/sell/sell.component.ts
@@ -6,4 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./sell.component.scss']
 })
 export class SellComponent {
+
+  public selectedTab: number = 0;
+  public tabLabels: Array<string> = ['orders', 'listings'];
+  //private resizeEventer: any;
+
+  filters: any = {
+    search:   '',
+    sort:     'DATE',
+    category: '*',
+    hashItems: ''
+  };
+
+  listing_sortings: Array<any> = [
+    { title: 'By title', value: 'TITLE' },
+    { title: 'By state', value: 'STATE' }
+  ];
+
+  listing_filtering: Array<any> = [
+    { title: 'All listings',  value: '' },
+    { title: 'Published',     value: true },
+    { title: 'Unpublished',   value: false }
+  ];
+
+
+
+
+  clear(): void {
+    this.filters = {
+      search:   '',
+      sort:     'DATE',
+      category: '*',
+      hashItems: ''
+    };
+    if (this.selectedTab === 1) {
+      //this.loadPage(0, true);
+    }
+  }
+
+  changeTab(index: number): void {
+    this.selectedTab = index;
+    this.clear();
+  }
+
 }
