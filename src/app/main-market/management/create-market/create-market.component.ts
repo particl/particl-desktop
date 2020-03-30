2b502d168124c1d3fcab719fc8ddef25820d7488
Martin Allien <martin@allien.work>
2020-03-30 18:47:05 +0200

    Manage > Create Market: layout & styling

--- src/app/main-market/management/create-market/create-market.component.ts
+++ src/app/main-market/management/create-market/create-market.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './create-market.component.html',
   styleUrls: ['./create-market.component.scss']
 })
 export class CreateMarketComponent {
+
 
 }
