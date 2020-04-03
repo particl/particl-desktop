11490724d6885fcdf74bb3e705a28bb85909338b
Martin Allien <martin@allien.work>
2020-04-03 15:10:57 +0200

    Orders: sidebar filtering tweaks + radio button group update

--- src/app/main-market/buy/buy.component.ts
+++ src/app/main-market/buy/buy.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
+// ----- ↓↓↓ ORDERS component ↓↓↓ ----- //
+export interface Filter {
+  value: string;
+  title: string;
+  count: string;
+}
+// ----- ↑↑↑ ORDERS component ↑↑↑ ----- //
 
 @Component({
   templateUrl: './buy.component.html',
@@ -13,6 +21,18 @@ export class BuyComponent {
   // ----- ↓↓↓ ORDERS component ↓↓↓ ----- //
   searchQuery: FormControl = new FormControl('');
   filterQuery: FormControl = new FormControl('all');
+
+  statusFilters: Filter[] = [
+    {value: 'all', title: 'All orders', count: '11'},
+    {value: 'bids', title: 'Bidding', count: '1'},
+    {value: 'awaiting', title: 'Awaiting payment', count: '2'},
+    {value: 'escrow', title: 'Escrow pending', count: '0'},
+    {value: 'packaging', title: 'Packaging', count: '1'},
+    {value: 'shipping', title: 'Shipping', count: '0'},
+    {value: 'complete', title: 'Completed', count: '3'},
+    {value: 'rejected', title: 'Rejected', count: '1'},
+    {value: 'cancelled', title: 'Cancelled', count: '3'}
+  ];
   // ----- ↑↑↑ ORDERS component ↑↑↑ ----- //
 
   public filters: any = {
