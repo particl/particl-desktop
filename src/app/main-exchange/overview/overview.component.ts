bf534f239098eb9fc8ac4cc8cd9a9390f96125af
Martin Allien <martin@allien.work>
2020-04-20 20:39:04 +0200

    Exchange > Overview: basics + filter sidebar

--- src/app/main-exchange/overview/overview.component.ts
+++ src/app/main-exchange/overview/overview.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExchangeOverviewComponent implements OnInit {
 
+  filters: any = {
+    search:   ''
+  };
+
+  exchange_status: Array<any> = [
+    { title: 'All exchanges', value: 'all',         amount: '3' },
+    { title: 'Active',        value: 'active',      amount: '1' },
+    { title: 'Completed',     value: 'completed',   amount: '2' },
+    { title: 'Cancelled',     value: 'cancelled',   amount: '0' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
