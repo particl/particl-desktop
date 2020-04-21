aa30332d9d146c114294588f4d64e2fa5904d83a
Martin Allien <martin@allien.work>
2020-04-21 18:23:07 +0200

    Exchange > Overview: exchange item layout
    + added more illustrations

--- src/app/main-exchange/overview/overview.component.ts
+++ src/app/main-exchange/overview/overview.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'lodash';
+import { DateFormatter } from 'app/core/util/utils';
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
 export class ExchangeOverviewComponent implements OnInit {
+
+  /* test only >>> */
+
+  public exchange: any = {
+    bot: {
+      name: 'SimpleSwap',
+      image: './assets/images/placeholder_4-3.jpg'
+    },
+    status: 'Active',
+    amount_from: 0.2,
+    amount_to: 2050,
+    currency_from: 'BTC',
+    currency_to: 'PART',
+    address_from: 'dujfnsdfnsafdnsk',
+    address_to: 'safdnskdujfnsdfn',
+    tx_from: 'bsjahdbfsadfdsfbsadifbsakdfaksdfsahdbfsajdhfbasjhdf',
+    tx_to: 'dsfbsadifbsakdfaksdfsahdbfsajdhfbasjhdfbsjahdbfsadf',
+    track_id: 'sdkfbsakfhdbaskjfd',
+    created_date: '2020-04-21 15:38:12',
+    updated_date: '2020-04-21 15:38:12',
+  }
+
+  /* << test only */
 
   filters: any = {
     search:   ''
@@ -21,6 +47,13 @@ export class ExchangeOverviewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formatDate(date: string): string {
+    if (!date) {
+      return '';
+    }
+    return new DateFormatter(new Date(date)).dateFormatter(false);
   }
 
 }
