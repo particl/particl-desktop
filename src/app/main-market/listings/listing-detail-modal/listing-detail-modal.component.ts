07d8896e72fa968a5439d890fb5a59ca8583c9bd
Martin Allien <martin@allien.work>
2020-04-23 18:37:17 +0200

    Market > Listings: listing detail modal styling, pt. II

--- src/app/main-market/listings/listing-detail-modal/listing-detail-modal.component.ts
+++ src/app/main-market/listings/listing-detail-modal/listing-detail-modal.component.ts
@@ -12,11 +12,20 @@ export class ListingDetailModalComponent implements OnInit {
     listing: {
       title: 'eGift Card - Mastercard ($250) USA',
       shortDescription: 'Digital Delivery (Replaces Tracking Number)',
-      longDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+      longDescription: 'I want to get these to people who need them! (Everyone) I paid a lot for these masks and I am only charging enough markup to allow me to make a bigger order next. Ships Priority Mail USPS, If post office closures happen I will switch to any provider still open with Priority shipping. I am unsure if I can get another shipment in but I will try if these sell quickly.\n\nI am actively seeking to provide more supplies like this. If you would like CASE Pricing or high qty please leave contact info in your private bid and I will contact you.\n\nDOMESTIC USA SHIPPING ONLY. I am unsure if you would recieve it in time otherwise.',
       imageCollection: {
         featuredImage: {
           thumbnail: './assets/images/placeholder_4-3.jpg'
-        }
+        },
+        images: [
+          './assets/images/placeholder_4-3.jpg',
+          './assets/images/placeholder_1-1.jpg',
+          './assets/images/placeholder_4-3.jpg',
+          './assets/images/placeholder_3-4.jpg',
+          './assets/images/placeholder_1-1.jpg',
+          './assets/images/placeholder_4-3.jpg',
+          './assets/images/placeholder_3-4.jpg'
+        ]
       },
       basePrice: 865,
       isAboutToExpire: true,
@@ -38,7 +47,7 @@ export class ListingDetailModalComponent implements OnInit {
         seller: 'sdfhbsjhdfb',
       },
       isFlagged: false,
-      isMine: false,
+      isMine: true,
       VoteDetails: {
         isReported: false,
       }
