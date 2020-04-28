56a210930a710b7bea7b9bc566f3224402754eae
Martin Allien <martin@allien.work>
2020-04-28 18:14:26 +0200

    Exchange > New: "choose coins" styling

--- src/app/main-exchange/new-exchange/new-exchange.component.ts
+++ src/app/main-exchange/new-exchange/new-exchange.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewExchangeComponent implements OnInit {
 
+  filters: any = {
+    search:   ''
+  };
+
+  supported_coins: Array<any> = [
+    // sort alphabetically
+    { title: 'Bitcoin',   value: 'btc', },
+    { title: 'DAI',       value: 'dai', },
+    { title: 'Horizen',   value: 'zen', },
+    { title: 'Particl',   value: 'part', }
+  ];
+
   constructor() { }
 
   ngOnInit() {
