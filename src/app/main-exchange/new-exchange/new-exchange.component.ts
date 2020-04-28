7faa65612bc39fb1523234e4e2e4a53f4f87549a
Martin Allien <martin@allien.work>
2020-04-28 19:15:18 +0200

    Exchange > New: "choose exchange"

--- src/app/main-exchange/new-exchange/new-exchange.component.ts
+++ src/app/main-exchange/new-exchange/new-exchange.component.ts
@@ -19,6 +19,13 @@ export class NewExchangeComponent implements OnInit {
     { title: 'Particl',   value: 'part', }
   ];
 
+  exchanges_list: Array<any> = [
+    // sort by best rate?
+    { title: 'SimpleSwap',  value: 'simpleswap',  amount: '0.5413',   rate: '145.5163',   tag: 'Best rate' },
+    { title: 'Changelly',   value: 'changelly',   amount: '0.5315',   rate: '146.5413',   tag: '' },
+    { title: 'StealthEx',   value: 'stealthex',   amount: '0.5138',   rate: '147.1052',   tag: '' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
