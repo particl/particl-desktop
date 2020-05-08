70c3b3f9d80aa96d7660d21cf79988f1a95f5284
Martin Allien <martin@allien.work>
2020-05-08 16:21:08 +0200

    Market > Questions: Q/A list item + threads added

--- src/app/main-market/shared/question-answer-thread/question-answer-thread.component.ts
+++ src/app/main-market/shared/question-answer-thread/question-answer-thread.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-question-answer-thread',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-answer-thread.component.scss']
 })
 export class QuestionAnswerThreadComponent implements OnInit {
+
+  @Input() type: string;
 
   constructor() { }
 
