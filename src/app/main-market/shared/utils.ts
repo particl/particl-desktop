d5eddaa7d634ee9ab6ab0f2e6240c142c18d532d
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2020-10-07 13:14:25 +0200

    [market] Updates particl-market dependency.

--- src/app/main-market/shared/utils.ts
+++ src/app/main-market/shared/utils.ts
@@ -17,10 +17,10 @@ export function parseImagePath(image: RespGeneralImageItem, version: IMAGE_VERSI
 
   // validate image actually exists... only supports 'FILE' type at the moment (or at least thats all we seem to be working with currently)
   // NB! commented out at present since the actual image datas might not be present on the image item (and therefore cannot be validated)
-  // const foundImg = image.ImageDatas.find(d => isBasicObjectType(d) && (d.imageVersion === version) && (d.protocol === 'FILE'));
-  // if (!foundImg) {
-  //   return '';
-  // }
+  const foundImg = image.ImageDatas.find(d => isBasicObjectType(d) && (d.protocol === 'FILE')); // && (d.imageVersion === version)
+  if (!foundImg) {
+    return '';
+  }
 
   return `${url}${url[url.length - 1] !== '/' ? '/' : ''}api/images/${+image.id}/${version}`;
 }
