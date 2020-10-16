7ffdaf85f795db636e8418eae8509fd8084eb144
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2020-10-16 13:58:49 +0200

    Removes old code from develop branch to avoid confusion

--- bin/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-yarn install
-ng serve --host 0.0.0.0 --port $PORT --env=$GUI_ENV
--- src/app/core-ui/main/release-notification/release-notification.model.ts
+++
@@ -1,7 +0,0 @@
-
-export class ReleaseNotification {
-  tag_name: string;
-  prerelease: string;
-  html_url: string;
-  body: string;
-};
