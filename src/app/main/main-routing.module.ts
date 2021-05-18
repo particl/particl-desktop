43c23de6fe5e66ab0b43f4d98004e031172861c8
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2021-05-18 15:34:42 +0200

    [general] Remove unused modules

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -17,7 +17,6 @@ const routes: Routes = [
       { path: 'extra', loadChildren: () => import('app/main-extra/extra.module').then(m => m.ExtraModule) },
       { path: 'wallet', loadChildren: () => import('app/main-wallet/wallet.module').then(m => m.WalletModule)},
       { path: 'market', loadChildren: () => import('app/main-market/market.module').then(m => m.MarketModule)},
-      { path: 'exchange', loadChildren: () => import('app/main-exchange/exchange.module').then(m => m.ExchangeModule)},
       { path: '', redirectTo: 'extra', pathMatch: 'full' },
     ]
   }
