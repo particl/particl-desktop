8df309a10802f4c29a5a362fe9aeaa8be0702285
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2021-06-15 11:23:59 +0200

    [governance] re-enable tree shaking of charts.

--- src/app/main-governance/shared/shared.module.ts
+++ src/app/main-governance/shared/shared.module.ts
@@ -7,27 +7,29 @@ import { NightingalesRoseChartComponent } from './charts/NightingalesRoseChart/c
 import { NgxEchartsModule } from 'ngx-echarts';
 
 // Import the echarts core module, which provides the necessary interfaces for using echarts.
-// import * as echarts from 'echarts/core';
-import * as echarts from 'echarts';
-// // Import specific chart types, all with Chart suffix
-// import {
-//     PieChart
-// } from 'echarts/charts';
-// import {
-//   TitleComponent,
-//   TooltipComponent,
-//   GridComponent
-// } from 'echarts/components';
-// // Import the Canvas renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
-// import {
-//   CanvasRenderer
-// } from 'echarts/renderers';
+import * as echarts from 'echarts/core';
+// import * as echarts from 'echarts';
+// Import specific chart types, all with Chart suffix
+import {
+    PieChart
+} from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  PolarComponent,
+  SingleAxisComponent
+} from 'echarts/components';
+// Import the Canvas renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+  CanvasRenderer
+} from 'echarts/renderers';
 
 
-// // define components for echarts to use: allows for treeshaking and importing of only the specific charts used
-// echarts.use(
-//   [TitleComponent, TooltipComponent, GridComponent, PieChart, CanvasRenderer]
-// );
+// define components for echarts to use: allows for treeshaking and importing of only the specific charts used
+echarts.use(
+  [TitleComponent, TooltipComponent, LegendComponent, PolarComponent, SingleAxisComponent, PieChart, CanvasRenderer]
+);
 
 
 @NgModule({
