ad28009d14f106f2a24dfbc01cf171ffae948b5d
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2022-10-13 14:44:11 +0200

    Adding in core status message and running indicator on welcome/home page

--- src/app/main-extra/welcome/welcome.component.ts
+++ src/app/main-extra/welcome/welcome.component.ts
@@ -5,6 +5,8 @@ import { takeUntil, tap } from 'rxjs/operators';
 import { MotdService, MessageQuote } from 'app/core/services/motd.service';
 import { Select } from '@ngxs/store';
 import { ApplicationConfigState } from 'app/core/app-global-state/app.state';
+import { RunningStatus } from 'app/networks/particl/particl.models';
+import { ParticlCoreState } from 'app/networks/particl/particl.state';
 
 
 @Component({
@@ -14,6 +16,11 @@ import { ApplicationConfigState } from 'app/core/app-global-state/app.state';
 export class WelcomeComponent implements OnDestroy {
 
   @Select(ApplicationConfigState.moduleVersions('app')) clientVersion$: Observable<string>;
+  @Select(ParticlCoreState.getStatusMessage) particlCoreStatus: Observable<string>;
+  @Select(ParticlCoreState.getStartedStatus) particlCoreStartedIndicator: Observable<RunningStatus>;
+
+  RunningStatuses: typeof RunningStatus = RunningStatus;
+
   motd: MessageQuote = {author: '', text: ''};
 
   private destroy$: Subject<void> = new Subject();
